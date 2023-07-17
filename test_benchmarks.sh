b63cd6471c35cf230dae5197df08c3afe6925770
MorellRAP <rezaalipour.usi@gmail.com>
2023-07-17 22:41:55 +0200

    test_benchmark.sh updated!

--- test_benchmarks.sh
+++ test_benchmarks.sh
@@ -3,18 +3,28 @@
 PYTHON='python3'
 SCRIPT='main.py'
 
-BENCH=$1
+echo -e "====================================
+USAGE:
+1- benchmark-path input/ver/abs_diff_i4_o3.ver
+2- Maximum LPP; e.g., 4
+3- Maximum PPO; e.g., 4
+4- ET; e.g., 2
+5- Partitioning-Percentage; e.g., 10
+===================================="
 
-for LPP in {1..4}
+BENCH=$1
+MAX_LPP=$2
+MAX_PPO=$3
+ET=$4
+PAP=$5
+
+for (( LPP=1; LPP<=$MAX_LPP; LPP++ ))
 do
-  for PPO in {1..4}
+  for (( PPO=1; PPO<=$MAX_PPO; PPO++ ))
   do
-    for ET in {2..2}
-    do
-      for PAP in {10..90..10}
-      do
-        $PYTHON $SCRIPT $BENCH -et=$ET -lpp=$LPP -ppo=$PPO -pap=$PAP
-      done
-    done
+  echo $PYTHON $SCRIPT $BENCH -et=$ET -lpp=$LPP -ppo=$PPO -pap=$PAP
+  $PYTHON $SCRIPT $BENCH -et=$ET -lpp=$LPP -ppo=$PPO -pap=$PAP
+
   done
 done
+
