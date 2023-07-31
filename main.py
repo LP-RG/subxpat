294c4c3c45237ddff8e9a3ac9fa2122f200769ce
MorellRAP <rezaalipour.usi@gmail.com>
2023-07-31 23:07:56 +0200

    minor debugging!

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import shutil
 import csv
-import Z3Log
+
 from Z3Log.verilog import Verilog
 from Z3Log.graph import Graph
 from Z3Log.utils import *
@@ -9,7 +9,7 @@ from Z3Log.config import path as z3logpath
 
 from sxpat.templateCreator import Template_SOP1
 from sxpat.templateSpecs import TemplateSpecs
-from sxpat.config.paths import *
+from sxpat.config import paths as sxpatpaths
 
 from sxpat.synthesis import Synthesis
 from sxpat.arguments import Arguments
@@ -20,11 +20,12 @@ def clean_all():
     directories = [z3logpath.OUTPUT_PATH['ver'][0], z3logpath.OUTPUT_PATH['aig'][0], z3logpath.OUTPUT_PATH['gv'][0],
                    z3logpath.OUTPUT_PATH['z3'][0],
                    z3logpath.OUTPUT_PATH['report'][0], z3logpath.OUTPUT_PATH['figure'][0], z3logpath.TEST_PATH['tb'][0],
-                   OUTPUT_PATH['area'][0], OUTPUT_PATH['power'][0], OUTPUT_PATH['delay'][0],
+                   sxpatpaths.OUTPUT_PATH['area'][0], sxpatpaths.OUTPUT_PATH['power'][0], sxpatpaths.OUTPUT_PATH['delay'][0],
                    z3logpath.LOG_PATH['yosys'][0],
-                   OUTPUT_PATH['json'][0]]
+                   sxpatpaths.OUTPUT_PATH['json'][0]]
 
     for directory in directories:
+
         if os.path.exists(directory):
             shutil.rmtree(directory)
         os.makedirs(directory, exist_ok=True)
@@ -35,13 +36,16 @@ def main():
 
 
     if args.clean:
+        print(f'cleaning...')
         clean_all()
 
     setup_folder_structure()
-    for key in OUTPUT_PATH.keys():
-        directory = OUTPUT_PATH[key][0]
+    for key in sxpatpaths.OUTPUT_PATH.keys():
+        directory = sxpatpaths.OUTPUT_PATH[key][0]
         if ~os.path.exists(directory):
             os.makedirs(directory, exist_ok=True)
+
+
     if args.multiple:
         n_o = int(re.search(f'.*o(\d+).*', args.benchmark_name).group(1))
         max_error = 2**(n_o-1)
@@ -50,6 +54,7 @@ def main():
         else:
             step = max_error // 8
             et_array = list(range(step, max_error + 1, step))
+
 
         for et in et_array:
             specs_obj = TemplateSpecs(name='Sop1', exact=args.benchmark_name, literals_per_product=args.lpp,
