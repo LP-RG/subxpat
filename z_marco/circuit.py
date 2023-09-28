47c048e01c0d2d1414f73ea02ceca3c073be6d07
MarcusTral <biasion.marco.2001@gmail.com>
2023-09-28 10:51:28 +0200

    added Gate class hierarchy and updated circuit class

--- z_marco/circuit.py
+++ z_marco/circuit.py
@@ -1,34 +1,83 @@
+from __future__ import annotations
+
+from typing import Dict, Iterable, Iterator, List, Tuple, Union
+
 import dataclasses as dc
-from functools import cached_property
 import io
 from itertools import chain
-import itertools
 import re
-from typing import Dict, Iterable, Iterator, List, Union
 import z3
+from sxpat.annotatedGraph import AnnotatedGraph
+
+
+@dc.dataclass(frozen=True)
+class Gate:
+    name: str
+    annotation: str
+    def to_z3(self) -> z3.ExprRef: ...
+
+
+@dc.dataclass(frozen=True)
+class UnaryGate(Gate):
+    child: Gate
+
+
+@dc.dataclass(frozen=True)
+class NaryGate(Gate):
+    children: Tuple[Gate]
+
+
+@dc.dataclass(frozen=True)
+class BoolLiteral(Gate):
+    def to_z3(self) -> z3.BoolRef:
+        return z3.Bool(self.name)
+
+
+@dc.dataclass(frozen=True)
+class Not(UnaryGate):
+    def to_z3(self) -> z3.BoolRef:
+        return z3.Not(self.child.to_z3())
+
+
+@dc.dataclass(frozen=True)
+class And(NaryGate):
+    def to_z3(self) -> z3.BoolRef:
+        return z3.And(*(g.to_z3() for g in self.children))
+
+
+@dc.dataclass(frozen=True)
+class Or(NaryGate):
+    def to_z3(self) -> z3.BoolRef:
+        return z3.Or(*(g.to_z3() for g in self.children))
 
 
 @dc.dataclass(frozen=True)
 class Circuit:
     _inputs: List[str]   # list of names of input gates
     _outputs: List[str]  # list of names of output gates
-    _gates: Dict[str, Union[z3.BoolRef, 'Circuit']]
+    _gates: Dict[str, Gate]
 
     @property
-    def inputs(self) -> Dict[str, z3.BoolRef]:
+    def inputs(self) -> Dict[str, Gate]:
         return {g: self._gates[g] for g in self._inputs}
 
     @property
-    def outputs(self) -> Dict[str, z3.BoolRef]:
+    def outputs(self) -> Dict[str, Gate]:
         return {g: self._gates[g] for g in self._outputs}
 
     @property
-    def internals(self) -> Dict[str, z3.BoolRef]:
-        return dict(filter(lambda g: g[0] not in chain(self._inputs, self._outputs), self._gates.items()))
+    def internals(self) -> Dict[str, Gate]:
+        in_out = set(chain(self._inputs, self._outputs))
+        return dict(filter(lambda g: g[0] not in in_out, self._gates.items()))
 
-    def normalized(self) -> 'Circuit':
+    def normalized(self) -> Circuit:
         """Returns a normalized version of the circuit."""
         raise "To be defined."
+
+
+def load_annotated(graph: AnnotatedGraph) -> Circuit:
+    print("LOADING MARCO AAAAAA")
+    print(type(graph.graph.nodes), graph.graph.nodes)
 
 
 def load_verilog(*, file_path: str = None, code: str = None) -> Circuit:
@@ -44,7 +93,7 @@ def load_verilog(*, file_path: str = None, code: str = None) -> Circuit:
 
     inputs: List[str] = list()   # list of names of input gates
     outputs: List[str] = list()  # list of names of output gates
-    all_gates: Dict[str, z3.BoolRef] = dict()
+    all_gates: Dict[str, Gate] = dict()
 
     for size, names in re.findall(INPUTS_PATTERN, code):
         # sanitize names
@@ -58,7 +107,7 @@ def load_verilog(*, file_path: str = None, code: str = None) -> Circuit:
             )))
         # add variables
         inputs.extend(names)
-        all_gates.update({n: z3.Bool(n) for n in names})
+        all_gates.update({n: BoolLiteral(n, None) for n in names})
 
     for size, names in re.findall(OUTPUTS_PATTERN, code):
         # sanitize names
@@ -79,21 +128,21 @@ def load_verilog(*, file_path: str = None, code: str = None) -> Circuit:
     return Circuit(inputs, outputs, all_gates)
 
 
-def parse_or(expr: str) -> z3.BoolRef:
+def parse_or(expr: str) -> Union[Or, Gate]:
     terms = [parse_and(group) for group in expr.split("|")]
     if len(terms) == 1:
         return terms[0]
     return z3.Or(*terms)
 
 
-def parse_and(expr: str) -> z3.BoolRef:
+def parse_and(expr: str) -> Union[And, Gate]:
     terms = [parse_not(group) for group in expr.split("&")]
     if len(terms) == 1:
         return terms[0]
     return z3.And(*terms)
 
 
-def parse_not(expr: str) -> z3.BoolRef:
+def parse_not(expr: str) -> Union[Not, Gate]:
     expr = devectorize_name(expr)
     if expr.startswith("~"):
         return z3.Not(z3.Bool(expr.strip("~")))
