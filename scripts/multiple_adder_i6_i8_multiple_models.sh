b8fb5b2e59b2d6ceef0f13d9ffc932e05c4f9636
MorellRAP <rezaalipour.usi@gmail.com>
2023-10-05 01:42:15 +0200

    other scripts are updated to adapt multiple models per iteration

---
+++ scripts/multiple_adder_i6_i8_multiple_models.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+PYTHON='python3'
+SCRIPT='main.py'
+
+ADDER_6='adder_i6_o4.v'
+for SIZE in 1 2 3 4 5 10 15 20 25 30 35 40 45 50
+do
+
+  $PYTHON $SCRIPT "input/ver/$ADDER_6" -lpp=6 -ppo=6 -et=8 --subxpat --grid -iterations=12 -app "input/ver/$ADDER_6" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10
+  $PYTHON $SCRIPT "input/ver/$ADDER_6" -lpp=6 -ppo=6 -et=7 --subxpat --grid -iterations=12 -app "input/ver/$ADDER_6" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10
+  $PYTHON $SCRIPT "input/ver/$ADDER_6" -lpp=6 -ppo=6 -et=6 --subxpat --grid -iterations=12 -app "input/ver/$ADDER_6" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10
+  $PYTHON $SCRIPT "input/ver/$ADDER_6" -lpp=6 -ppo=6 -et=5 --subxpat --grid -iterations=12 -app "input/ver/$ADDER_6" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10
+  $PYTHON $SCRIPT "input/ver/$ADDER_6" -lpp=6 -ppo=6 -et=4 --subxpat --grid -iterations=12 -app "input/ver/$ADDER_6" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10
+  $PYTHON $SCRIPT "input/ver/$ADDER_6" -lpp=6 -ppo=6 -et=3 --subxpat --grid -iterations=12 -app "input/ver/$ADDER_6" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10
+  $PYTHON $SCRIPT "input/ver/$ADDER_6" -lpp=6 -ppo=6 -et=2 --subxpat --grid -iterations=12 -app "input/ver/$ADDER_6" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10
+  $PYTHON $SCRIPT "input/ver/$ADDER_6" -lpp=6 -ppo=6 -et=1 --subxpat --grid -iterations=12 -app "input/ver/$ADDER_6" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10
+done
+
+ADDER_8='adder_i8_o5.v'
+for SIZE in 1 2 3 4 5 10 15 20 25 30 35 40 45 50
+do
+  $PYTHON $SCRIPT "input/ver/$ADDER_8" -lpp=8 -ppo=8 -et=16 --subxpat --grid -iterations=16 -app "input/ver/$ADDER_8" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10
+  $PYTHON $SCRIPT "input/ver/$ADDER_8" -lpp=8 -ppo=8 -et=14 --subxpat --grid -iterations=16 -app "input/ver/$ADDER_8" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10
+  $PYTHON $SCRIPT "input/ver/$ADDER_8" -lpp=8 -ppo=8 -et=12 --subxpat --grid -iterations=16 -app "input/ver/$ADDER_8" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10
+  $PYTHON $SCRIPT "input/ver/$ADDER_8" -lpp=8 -ppo=8 -et=10 --subxpat --grid -iterations=16 -app "input/ver/$ADDER_8" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10
+  $PYTHON $SCRIPT "input/ver/$ADDER_8" -lpp=8 -ppo=8 -et=8 --subxpat --grid -iterations=16 -app "input/ver/$ADDER_8" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10
+  $PYTHON $SCRIPT "input/ver/$ADDER_8" -lpp=8 -ppo=8 -et=6 --subxpat --grid -iterations=16 -app "input/ver/$ADDER_8" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10
+  $PYTHON $SCRIPT "input/ver/$ADDER_8" -lpp=8 -ppo=8 -et=4 --subxpat --grid -iterations=16 -app "input/ver/$ADDER_8" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10
+  $PYTHON $SCRIPT "input/ver/$ADDER_8" -lpp=8 -ppo=8 -et=2 --subxpat --grid -iterations=16 -app "input/ver/$ADDER_8" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10
+done
---
+++ scripts/multiple_madd_i6_i9_multiple_models.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+PYTHON='python3'
+SCRIPT='main.py'
+
+
+MADD_6='madd_i6_o4.v'
+for SIZE in 1 2 3 4
+do
+  $PYTHON $SCRIPT "input/ver/$MADD_6" -lpp=6 -ppo=6 -et=8 --subxpat --grid -iterations=12 -app "input/ver/$MADD_6" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10
+  $PYTHON $SCRIPT "input/ver/$MADD_6" -lpp=6 -ppo=6 -et=7 --subxpat --grid -iterations=12 -app "input/ver/$MADD_6" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10
+  $PYTHON $SCRIPT "input/ver/$MADD_6" -lpp=6 -ppo=6 -et=6 --subxpat --grid -iterations=12 -app "input/ver/$MADD_6" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10
+  $PYTHON $SCRIPT "input/ver/$MADD_6" -lpp=6 -ppo=6 -et=5 --subxpat --grid -iterations=12 -app "input/ver/$MADD_6" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10
+  $PYTHON $SCRIPT "input/ver/$MADD_6" -lpp=6 -ppo=6 -et=4 --subxpat --grid -iterations=12 -app "input/ver/$MADD_6" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10
+  $PYTHON $SCRIPT "input/ver/$MADD_6" -lpp=6 -ppo=6 -et=3 --subxpat --grid -iterations=12 -app "input/ver/$MADD_6" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10
+  $PYTHON $SCRIPT "input/ver/$MADD_6" -lpp=6 -ppo=6 -et=2 --subxpat --grid -iterations=12 -app "input/ver/$MADD_6" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10
+  $PYTHON $SCRIPT "input/ver/$MADD_6" -lpp=6 -ppo=6 -et=1 --subxpat --grid -iterations=12 -app "input/ver/$MADD_6" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10
+done
+
+MADD_9='madd_i9_o6.v'
+for SIZE in 1 2 3 4
+do
+  $PYTHON $SCRIPT "input/ver/$MADD_9" -lpp=9 -ppo=9 -et=32 --subxpat --grid -iterations=18 -app "input/ver/$MADD_9" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10
+  $PYTHON $SCRIPT "input/ver/$MADD_9" -lpp=9 -ppo=9 -et=28 --subxpat --grid -iterations=18 -app "input/ver/$MADD_9" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10
+  $PYTHON $SCRIPT "input/ver/$MADD_9" -lpp=9 -ppo=9 -et=24 --subxpat --grid -iterations=18 -app "input/ver/$MADD_9" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10
+  $PYTHON $SCRIPT "input/ver/$MADD_9" -lpp=9 -ppo=9 -et=20 --subxpat --grid -iterations=18 -app "input/ver/$MADD_9" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10
+  $PYTHON $SCRIPT "input/ver/$MADD_9" -lpp=9 -ppo=9 -et=16 --subxpat --grid -iterations=18 -app "input/ver/$MADD_9" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10
+  $PYTHON $SCRIPT "input/ver/$MADD_9" -lpp=9 -ppo=9 -et=12 --subxpat --grid -iterations=18 -app "input/ver/$MADD_9" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10
+  $PYTHON $SCRIPT "input/ver/$MADD_9" -lpp=9 -ppo=9 -et=8 --subxpat --grid -iterations=18 -app "input/ver/$MADD_9" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10
+  $PYTHON $SCRIPT "input/ver/$MADD_9" -lpp=9 -ppo=9 -et=4 --subxpat --grid -iterations=18 -app "input/ver/$MADD_9" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10
+done
