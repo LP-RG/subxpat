25101e36e69cccdc277cd9416764d46a9eb11744
MarcusTral <biasion.marco.2001@gmail.com>
2023-10-19 13:42:22 +0200

    created folder for runner creators, and implemented subxpat as child class of a new RunnerCreator parent class

---
+++ sxpat/runner_creator/__init__.py
@@ -0,0 +1,10 @@
+
+# super class
+from .runner_creator import RunnerCreator
+
+# classes
+from .xpat_creator import XPatRunnerCreator
+# from .xpat_creator import XPatRunnerCreator
+
+# factory
+from .factory import RunnerCreatorFactory
