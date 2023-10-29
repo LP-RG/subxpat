7b43e897e9ee5d2e9d3f14901ea23fdf65500f80
MorellRAP <rezaalipour.usi@gmail.com>
2023-10-29 18:57:24 +0100

    minor

---
+++ multiple_abs_i6_i8_shared_subxpat.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+PYTHON='python3'
+SCRIPT='main.py'
+
+
+ABS_6='abs_diff_i6_o4.v'
+for SIZE in 1 2 3 4 5 10 15 20 25 30 35 40 45 50
+do
+  $PYTHON $SCRIPT "input/ver/$ABS_6" -lpp=6 -pit=6 -et=8 --subxpat --grid -iterations=12 -app "input/ver/$ABS_6" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10 --shared
+  $PYTHON $SCRIPT "input/ver/$ABS_6" -lpp=6 -pit=6 -et=7 --subxpat --grid -iterations=12 -app "input/ver/$ABS_6" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10 --shared
+  $PYTHON $SCRIPT "input/ver/$ABS_6" -lpp=6 -pit=6 -et=6 --subxpat --grid -iterations=12 -app "input/ver/$ABS_6" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10 --shared
+  $PYTHON $SCRIPT "input/ver/$ABS_6" -lpp=6 -pit=6 -et=5 --subxpat --grid -iterations=12 -app "input/ver/$ABS_6" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10 --shared
+  $PYTHON $SCRIPT "input/ver/$ABS_6" -lpp=6 -pit=6 -et=4 --subxpat --grid -iterations=12 -app "input/ver/$ABS_6" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10 --shared
+  $PYTHON $SCRIPT "input/ver/$ABS_6" -lpp=6 -pit=6 -et=3 --subxpat --grid -iterations=12 -app "input/ver/$ABS_6" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10 --shared
+  $PYTHON $SCRIPT "input/ver/$ABS_6" -lpp=6 -pit=6 -et=2 --subxpat --grid -iterations=12 -app "input/ver/$ABS_6" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10 --shared
+  $PYTHON $SCRIPT "input/ver/$ABS_6" -lpp=6 -pit=6 -et=1 --subxpat --grid -iterations=12 -app "input/ver/$ABS_6" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10 --shared
+done
+
+ABS_8='abs_diff_i8_o5.v'
+for SIZE in 1 2 3 4 5 10 15 20 25 30 35 40 45 50
+do
+  $PYTHON $SCRIPT "input/ver/$ABS_8" -lpp=8 -pit=8 -et=16 --subxpat --grid -iterations=16 -app "input/ver/$ABS_8" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10 --shared
+  $PYTHON $SCRIPT "input/ver/$ABS_8" -lpp=8 -pit=8 -et=14 --subxpat --grid -iterations=16 -app "input/ver/$ABS_8" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10 --shared
+  $PYTHON $SCRIPT "input/ver/$ABS_8" -lpp=8 -pit=8 -et=12 --subxpat --grid -iterations=16 -app "input/ver/$ABS_8" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10 --shared
+  $PYTHON $SCRIPT "input/ver/$ABS_8" -lpp=8 -pit=8 -et=10 --subxpat --grid -iterations=16 -app "input/ver/$ABS_8" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10 --shared
+  $PYTHON $SCRIPT "input/ver/$ABS_8" -lpp=8 -pit=8 -et=8 --subxpat --grid -iterations=16 -app "input/ver/$ABS_8" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10 --shared
+  $PYTHON $SCRIPT "input/ver/$ABS_8" -lpp=8 -pit=8 -et=6 --subxpat --grid -iterations=16 -app "input/ver/$ABS_8" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10 --shared
+  $PYTHON $SCRIPT "input/ver/$ABS_8" -lpp=8 -pit=8 -et=4 --subxpat --grid -iterations=16 -app "input/ver/$ABS_8" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10 --shared
+  $PYTHON $SCRIPT "input/ver/$ABS_8" -lpp=8 -pit=8 -et=2 --subxpat --grid -iterations=16 -app "input/ver/$ABS_8" -mode=3 -subgraphsize="$SIZE" -population=4 -num_models=10 --shared
+done
