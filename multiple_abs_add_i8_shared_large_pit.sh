9f7b4c325146f35b654660f1d9a30957395844ee
MorellRAP <rezaalipour.usi@gmail.com>
2023-11-15 23:22:12 +0100

    scripts added for xpat vs shared 1 model!

---
+++ multiple_abs_add_i8_shared_large_pit.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+PYTHON='python3'
+SCRIPT='main.py'
+
+
+ADD_8='adder_i8_o5.v'
+ABS_8='abs_diff_i8_o5.v'
+ADD_4='adder_i4_o3.v'
+ABS_4='abs_diff_i4_o3.v'
+
+$PYTHON $SCRIPT "input/ver/$ADD_4" -lpp=4 -pit=12 -et=4  --grid  -app "input/ver/$ADD_4"  --shared --clean
+$PYTHON $SCRIPT "input/ver/$ADD_4" -lpp=4 -pit=12 -et=3  --grid  -app "input/ver/$ADD_4"   --shared
+$PYTHON $SCRIPT "input/ver/$ADD_4" -lpp=4 -pit=12 -et=2  --grid  -app "input/ver/$ADD_4"   --shared
+$PYTHON $SCRIPT "input/ver/$ADD_4" -lpp=4 -pit=12 -et=1  --grid  -app "input/ver/$ADD_4"   --shared
+
+
+$PYTHON $SCRIPT "input/ver/$ABS_4" -lpp=4 -pit=12 -et=4  --grid  -app "input/ver/$ABS_4"   --shared
+$PYTHON $SCRIPT "input/ver/$ABS_4" -lpp=4 -pit=12 -et=3  --grid  -app "input/ver/$ABS_4"   --shared
+$PYTHON $SCRIPT "input/ver/$ABS_4" -lpp=4 -pit=12 -et=2  --grid  -app "input/ver/$ABS_4"   --shared
+$PYTHON $SCRIPT "input/ver/$ABS_4" -lpp=4 -pit=12 -et=1  --grid  -app "input/ver/$ABS_4"   --shared
+
+
+$PYTHON $SCRIPT "input/ver/$ADD_8" -lpp=8 -pit=40 -et=16  --grid  -app "input/ver/$ADD_8"  --shared
+$PYTHON $SCRIPT "input/ver/$ADD_8" -lpp=8 -pit=40 -et=14  --grid  -app "input/ver/$ADD_8"   --shared
+$PYTHON $SCRIPT "input/ver/$ADD_8" -lpp=8 -pit=40 -et=12  --grid  -app "input/ver/$ADD_8"   --shared
+$PYTHON $SCRIPT "input/ver/$ADD_8" -lpp=8 -pit=40 -et=10  --grid  -app "input/ver/$ADD_8"   --shared
+$PYTHON $SCRIPT "input/ver/$ADD_8" -lpp=8 -pit=40 -et=8  --grid  -app "input/ver/$ADD_8"   --shared
+$PYTHON $SCRIPT "input/ver/$ADD_8" -lpp=8 -pit=40 -et=6  --grid  -app "input/ver/$ADD_8"   --shared
+$PYTHON $SCRIPT "input/ver/$ADD_8" -lpp=8 -pit=40 -et=4  --grid  -app "input/ver/$ADD_8"   --shared
+$PYTHON $SCRIPT "input/ver/$ADD_8" -lpp=8 -pit=40 -et=2  --grid  -app "input/ver/$ADD_8"   --shared
+
+$PYTHON $SCRIPT "input/ver/$ABS_8" -lpp=8 -pit=40 -et=16  --grid  -app "input/ver/$ABS_8"   --shared
+$PYTHON $SCRIPT "input/ver/$ABS_8" -lpp=8 -pit=40 -et=14  --grid  -app "input/ver/$ABS_8"   --shared
+$PYTHON $SCRIPT "input/ver/$ABS_8" -lpp=8 -pit=40 -et=12  --grid  -app "input/ver/$ABS_8"   --shared
+$PYTHON $SCRIPT "input/ver/$ABS_8" -lpp=8 -pit=40 -et=10  --grid  -app "input/ver/$ABS_8"   --shared
+$PYTHON $SCRIPT "input/ver/$ABS_8" -lpp=8 -pit=40 -et=8  --grid  -app "input/ver/$ABS_8"   --shared
+$PYTHON $SCRIPT "input/ver/$ABS_8" -lpp=8 -pit=40 -et=6  --grid  -app "input/ver/$ABS_8"   --shared
+$PYTHON $SCRIPT "input/ver/$ABS_8" -lpp=8 -pit=40 -et=4  --grid  -app "input/ver/$ABS_8"   --shared
+$PYTHON $SCRIPT "input/ver/$ABS_8" -lpp=8 -pit=40 -et=2  --grid  -app "input/ver/$ABS_8"   --shared
