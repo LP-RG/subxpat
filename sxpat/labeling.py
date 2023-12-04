b486372ef59a7643289220f7994c43a517351a0a
MarcusTral <biasion.marco.2001@gmail.com>
2023-12-04 16:27:45 +0100

    partial merge from new master

--- sxpat/labeling.py
+++ sxpat/labeling.py
@@ -1,4 +1,7 @@
 import csv
+import os
+import shutil
+
 from colorama import Style, Fore
 from typing import Dict
 from Z3Log.verilog import Verilog
@@ -7,7 +10,46 @@ from Z3Log.utils import *
 from Z3Log.z3solver import Z3solver
 from Z3Log.config import path as z3logpath
 
-def labeling(exact_benchmark_name: str, approximate_benchmark: str, constant_value: 0) -> Dict:
+
+def labeling(exact_benchmark_name: str, approximate_benchmark: str, min_labeling: bool = False, parallel: bool = True) -> Dict:
+    verilog_obj_exact = Verilog(exact_benchmark_name)
+    verilog_obj_exact.export_circuit()
+
+    verilog_obj_approx = Verilog(approximate_benchmark)
+    verilog_obj_approx.export_circuit()
+
+    convert_verilog_to_gv(exact_benchmark_name)
+    convert_verilog_to_gv(approximate_benchmark)
+
+    # 2) convert clean exact and approximate circuits into their gv formats
+    graph_obj_exact = Graph(exact_benchmark_name)
+    graph_obj_approx = Graph(approximate_benchmark)
+
+    graph_obj_exact.export_graph()
+    graph_obj_approx.export_graph()
+
+    # convert gv to z3 expression
+    if min_labeling:
+        print(Fore.LIGHTBLUE_EX + f'min labeling...' + Style.RESET_ALL)
+        z3py_obj = Z3solver(exact_benchmark_name, approximate_benchmark, experiment=SINGLE, optimization=MAXIMIZE, style='min', parallel=parallel)
+    else:
+        print(Fore.LIGHTBLUE_EX + f'max labeling...' + Style.RESET_ALL)
+        z3py_obj = Z3solver(exact_benchmark_name, approximate_benchmark, experiment=SINGLE, optimization=MAXIMIZE, parallel=parallel)
+
+    z3py_obj.run_implicit_labeling()
+
+    labels = {}
+    for key in z3py_obj.labels.keys():
+        weight = z3py_obj.labels[key]
+        if key.startswith('app_'):
+            key = key.replace('app_', "")
+        labels[key] = weight
+    return labels
+
+
+# TODO: Deprecated
+def labeling_old(exact_benchmark_name: str, approximate_benchmark: str, constant_value: 0, min_labeling: bool) -> Dict:
+    """ ~ DEPRECATED ~ """
     # 1) create a clean verilog out of exact and approximate circuits
     verilog_obj_exact = Verilog(exact_benchmark_name)
     verilog_obj_exact.export_circuit()
@@ -26,24 +68,44 @@ def labeling(exact_benchmark_name: str, approximate_benchmark: str, constant_val
     graph_obj_approx.export_graph()
 
     # convert gv to z3 expression
-    z3py_obj = Z3solver(exact_benchmark_name, approximate_benchmark, experiment=SINGLE)
+    if min_labeling:
+        z3py_obj = Z3solver(exact_benchmark_name, approximate_benchmark, experiment=SINGLE, optimization=MAXIMIZE, style='min')
+    else:
+        z3py_obj = Z3solver(exact_benchmark_name, approximate_benchmark, experiment=SINGLE, optimization=MAXIMIZE)
 
     if constant_value == 0:
+
         labels_false = z3py_obj.label_circuit(False)
+        z3_folder, _ = OUTPUT_PATH['z3']
+        report_folder, _ = OUTPUT_PATH['report']
+        all_files = [f for f in os.listdir(z3_folder)]
+        # print(f'{all_files = }')
+        for dir in all_files:
+            if os.path.isdir(f'{z3_folder}/{dir}') and re.search('labeling', dir):
+                shutil.rmtree(f'{z3_folder}/{dir}')
+
+        all_files = [f for f in os.listdir(report_folder)]
+        for dir in all_files:
+            if os.path.isdir(f'{report_folder}/{dir}') and re.search('labeling', dir):
+                shutil.rmtree(f'{report_folder}/{dir}')
+
         return labels_false, labels_false
     elif constant_value == 1:
         labels_true = z3py_obj.label_circuit(True)
+        folder, _ = OUTPUT_PATH['z3']
+        all_files = [f for f in os.listdir(folder)]
+
+        for dir in all_files:
+            if os.path.isdir(f'{folder}/{dir}') and re.search('labeling', dir):
+                shutil.rmtree(f'{folder}/{dir}')
         return labels_true, labels_true
     else:
         labels_false = z3py_obj.label_circuit(False)
         labels_true = z3py_obj.label_circuit(True)
+        folder, _ = OUTPUT_PATH['z3']
+        all_files = [f for f in os.listdir(folder)]
+
+        for dir in all_files:
+            if os.path.isdir(f'{folder}/{dir}') and re.search('labeling', dir):
+                shutil.rmtree(f'{folder}/{dir}')
         return labels_true, labels_false
-
-
-
-
-
-if __name__ == "__main__":
-    pass
-else:
-    pass
