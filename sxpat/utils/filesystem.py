6f8a345919de1626799cd78a29a2c490617c5db9
MarcusTral <biasion.marco.2001@gmail.com>
2024-02-08 11:26:21 +0100

    updated filesystem util and usage

--- sxpat/utils/filesystem.py
+++ sxpat/utils/filesystem.py
@@ -1,61 +1,56 @@
 import os
+import shutil
 import tempfile
 
 
 class FS:
     """NOT YET USED"""
     @classmethod
-    def makedir(cls, path: str) -> None:
+    def mkdir(cls, path: str) -> None:
         """Recursively create the directory. Does nothing if the directory already exists."""
+        path = os.path.normpath(path)
+
         os.makedirs(path, exist_ok=True)
+
+    @classmethod
+    def rmdir(cls, path: str, recursive: bool = False) -> None:
+        """Remove the directory (recursively if wanted)."""
+        path = os.path.normpath(path)
+
+        (shutil.rmtree if recursive else os.rmdir)(path)
 
     @classmethod
     def listdir(cls, path: str):
         """Returns a list of paths representing all files in the given folder."""
         path = os.path.normpath(path)
-        return [f"{path}/{file}" for file in os.listdir(path)]
+
+        return [
+            os.path.relpath(f"{path}/{file}")
+            for file in os.listdir(path)
+        ]
 
     @classmethod
     def open(cls, path: str, mode: str):
-        folder, _ = os.path.split(path)
-        cls.create_directory(folder)
+        path = os.path.normpath(path)
+        directory = os.path.dirname(path)
+
+        cls.mkdir(directory)
         return open(path, mode)
 
+    @classmethod
+    def open_tmp(directory: str = None, delete: bool = False, binary: bool = False):
+        """ Create a temporary file on the filesystem. \n
+            If `directory` is given, the file will be created in that directory (created if missing). \n
+            If `delete` is `True` the file will be deleted once it is closed. \n
+            If `binary` is `True` the file will be opened in binary mode.
+        """
+        if directory is not None:
+            directory = os.path.normpath(directory)
+            FS.mkdir(directory)
 
-def create_directory(path: str) -> None:
-    """Recursively create the directory. Does nothing if the directory already exists."""
-    os.makedirs(path, exist_ok=True)
-
-
-def open_file(path: str, mode: str):
-    """Open the wanted file, creating all necessary folders in the path"""
-    directory = os.path.dirname(path)
-    create_directory(directory)
-    return open(path, mode)
-
-
-def listdir(path: str):
-    """Returns a list of paths representing all files in the given folder."""
-    path = os.path.normpath(path)
-    return [
-        os.path.relpath(f"{path}/{file}")
-        for file in os.listdir(path)
-    ]
-
-
-def get_temporary_file(directory: str = None, delete: bool = False, binary: bool = False):
-    """ Create a temporary file on the filesystem. \n
-        If `directory` is given, the file will be created in that directory (created if missing). \n
-        If `delete` is `True` the file will be deleted once it is closed. \n
-        If `binary` is `True` the file will be opened in binary mode.
-    """
-
-    if directory is not None:
-        create_directory(directory)
-
-    # create temporary file
-    return tempfile.NamedTemporaryFile(
-        mode='w+b' if binary else 'w+',
-        dir=directory,
-        delete=delete
-    )
+        # create temporary file
+        return tempfile.NamedTemporaryFile(
+            mode='w+b' if binary else 'w+',
+            dir=directory,
+            delete=delete
+        )
