1ea0666f51ab7b16ff9843cec0eae84cd68aa59b
MarcusTral <biasion.marco.2001@gmail.com>
2024-02-21 16:19:01 +0100

    minor changes to main.py, minor comments

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
-import shutil
-import csv
-from colorama import Fore, Style
+# import shutil
+# import csv
+# from colorama import Fore, Style
 
-from Z3Log.verilog import Verilog
-from Z3Log.graph import Graph
-from Z3Log.utils import *
-from Z3Log.z3solver import Z3solver
+# from Z3Log.verilog import Verilog
+# from Z3Log.graph import Graph
+# from Z3Log.utils import *
+from Z3Log.utils import setup_folder_structure
+# from Z3Log.z3solver import Z3solver
 from Z3Log.config import path as z3logpath
 
-from sxpat.templateCreator import Template_SOP1
+# from sxpat.templateCreator import Template_SOP1
 from sxpat.templateSpecs import TemplateSpecs
 from sxpat.config import paths as sxpatpaths
-from sxpat.config import config as sxpatconfig
+# from sxpat.config import config as sxpatconfig
 
-from sxpat.synthesis import Synthesis
+# from sxpat.synthesis import Synthesis
 from sxpat.arguments import Arguments
 
 from sxpat.xplore import explore_grid  # , explore_grid_shared
-from sxpat.stats import Stats, Result
+from sxpat.stats import Stats  # , Result
 
 from z_marco.utils import pprint
 from sxpat.utils.filesystem import FS
@@ -50,9 +51,11 @@ def main():
         for (directory, _) in sxpatpaths.OUTPUT_PATH.values():
             FS.mkdir(directory)
 
-        specs_obj = TemplateSpecs(name='Sop1' if not args.shared else 'SharedLogic', exact=args.benchmark_name, literals_per_product=args.lpp,
-                                  products_per_output=args.ppo,
-                                  benchmark_name=args.approximate_benchmark, num_of_models=args.num_models, subxpat=args.subxpat,
+        # todo:later: update how we create the templatespecs (more than 2 names, ecc.)
+        specs_obj = TemplateSpecs(name='Sop1' if not args.shared else 'SharedLogic', exact=args.benchmark_name,
+                                  literals_per_product=args.lpp, products_per_output=args.ppo,
+                                  benchmark_name=args.approximate_benchmark, num_of_models=args.num_models,
+                                  subxpat=args.subxpat, subxpat_v2=args.subxpat_v2,
                                   et=args.et,
                                   partitioning_percentage=args.partitioning_percentage, iterations=args.iterations,
                                   grid=args.grid, imax=args.imax, omax=args.omax, sensitivity=args.sensitivity,
@@ -64,7 +67,7 @@ def main():
             stats_obj = explore_grid(specs_obj)
         else:
             # todo:question: What should happen here?
-            raise
+            raise RuntimeError('WIP: for now --grid must be passed')
 
 
 def clean_all():
