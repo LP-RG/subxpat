2cd055b4671f89553323caef015496b3880ffe0a
Francesco <costafr@usi.ch>
2024-05-10 11:28:37 +0200

    Added implementation of smart ascending and smart descending et partitioning

--- sxpat/verification.py
+++ sxpat/verification.py
@@ -93,3 +93,42 @@ def erroreval_verification_explicit(exact_benchmark_name: str, approximate_bench
                                     'w') as f:
                                 pass
                             return False
+                        
+def erroreval_verification_wce(exact_benchmark_name: str, approximate_benchmark: str, et: int) -> int:
+
+    # print(f'{approximate_benchmark = }')
+    verilog_obj_exact = Verilog(exact_benchmark_name)
+    verilog_obj_exact.export_circuit()
+    #
+
+    verilog_obj_approx = Verilog(approximate_benchmark)
+    verilog_obj_approx.export_circuit()
+    #
+    convert_verilog_to_gv(exact_benchmark_name)
+    convert_verilog_to_gv(approximate_benchmark)
+    #
+    graph_obj_exact = Graph(exact_benchmark_name)
+    graph_obj_approx = Graph(approximate_benchmark)
+    #
+    graph_obj_exact.export_graph()
+    graph_obj_approx.export_graph()
+
+    z3py_obj_qor = Z3solver(exact_benchmark_name, approximate_benchmark)
+    z3py_obj_qor.convert_gv_to_z3pyscript_maxerror_qor()
+
+    z3py_obj_qor.export_z3pyscript()
+    z3py_obj_qor.run_z3pyscript_qor()
+
+    # Compare the obtained WCE
+    folder, extension = z3logpath.OUTPUT_PATH['report']
+    for csvfile in os.listdir(folder):
+        if csvfile.endswith(extension) and re.search(approximate_benchmark, csvfile):
+            report_file = f'{folder}/{csvfile}'
+            with open(report_file, 'r') as rf:
+                csvreader = csv.reader(rf)
+                for row in csvreader:
+                    if row[0] == WCE:
+                        obtained_wce = int(row[1])
+                        os.remove(report_file)
+
+    return obtained_wce
