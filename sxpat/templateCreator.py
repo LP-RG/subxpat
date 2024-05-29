01a9003e6302021b0a00470dfe9bcb311ddb3818
MorellRAP <rezaalipour.usi@gmail.com>
2024-05-29 07:25:41 +0200

    nothing

--- sxpat/templateCreator.py
+++ sxpat/templateCreator.py
@@ -269,7 +269,7 @@ class Template_SOP1(TemplateCreator):
         self.__json_in_path = this_json_path
 
     def label_graph(self, min_labeling: bool = False,  partial: bool = False, et: int = -1, parallel: bool = False):
-        print(f'{et = } for partial labeling!')
+        # print(f'{et = } for partial labeling!')
         # labels = labeling(self.exact_benchmark, self.benchmark_name, min_labeling, parallel)
         labels, _ = labeling_explicit(self.exact_benchmark, self.benchmark_name, constant_value=0, min_labeling=min_labeling,
                                       partial=partial, et=et, parallel=parallel)
