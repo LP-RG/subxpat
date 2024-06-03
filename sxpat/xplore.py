64d73669567da4004a299131750f1c53fcab3008
MorellRAP <rezaalipour.usi@gmail.com>
2024-06-03 22:41:59 +0200

    exiting in case of a loop detection!

--- sxpat/xplore.py
+++ sxpat/xplore.py
@@ -1,7 +1,7 @@
 import csv
 import time
 from typing import Iterable, Iterator, List, Union
-
+import networkx as nx
 from tabulate import tabulate
 
 import math
@@ -27,6 +27,7 @@ from z_marco.utils import pprint, color
 
 def explore_grid(specs_obj: TemplateSpecs):
 
+    previous_subgraphs = []
     print(f'{specs_obj = }')
 
     labeling_time: float = -1
@@ -140,6 +141,8 @@ def explore_grid(specs_obj: TemplateSpecs):
             # extract subgraph
             t_start = time.time()
             subgraph_is_available = template_obj.current_graph.extract_subgraph(specs_obj)
+
+            previous_subgraphs.append(template_obj.current_graph.subgraph)
             subgraph_extraction_time = time.time() - t_start
             subgraph_number_inputs = template_obj.current_graph.subgraph_num_inputs
             subgraph_number_outputs = template_obj.current_graph.subgraph_num_outputs
@@ -372,7 +375,21 @@ def explore_grid(specs_obj: TemplateSpecs):
 
         if exists_an_area_zero(current_population):
             break
-    display_the_tree(total)
+
+        loop_detected = False
+        for idx, s in enumerate(previous_subgraphs):
+            if idx == len(previous_subgraphs) - 1 and idx > 1:
+                if nx.utils.graphs_equal(previous_subgraphs[idx-2], previous_subgraphs[idx-1]) and \
+                    nx.utils.graphs_equal(previous_subgraphs[idx-2], previous_subgraphs[idx-3]):
+                    print(f'The last three subgraphs are equal')
+                    pprint.info3(f'The last three subgraphs are equal!')
+                    pprint.info3(f'Terminating the exploration!')
+                    loop_detected = True
+                    break
+        if loop_detected:
+            break
+
+    # display_the_tree(total)
 
     stats_obj.store_grid()
     return stats_obj
