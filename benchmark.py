13bbfa005a43fd5fb2b77df54c1b38be94e20493
atwim <atwim@usi.ch>
2024-06-04 11:42:52 +0200

    added scatter plots for visualizing the benchmark results

--- benchmark.py
+++ benchmark.py
@@ -9,20 +9,18 @@ import main
 import subprocess
 from Z3Log.config import path as z3logpath
 import matplotlib.pyplot as plt
+
 import numpy as np
 
 
-# argv[1] = benchmarks
-# argv[2] = metric_cols
-
-def run_benchmark(benchmarks=["abs_diff_i4_o2"], metric_cols=[5]):
+def run_benchmark(benchmarks=["abs_diff_i4_o2"], metric_cols=[5], scatter_plots=False):
 
 
 
 
     num_et_points = 8
     num_iterations = 10
-    num_models = 1000
+    num_models = 100
     mode_num = 3
 
     ET_points = [1, 2, 3, 4]
@@ -54,24 +52,24 @@ def run_benchmark(benchmarks=["abs_diff_i4_o2"], metric_cols=[5]):
         ppo = num_inputs * 2
         lpp = num_inputs
 
-        # for et in ET_points:
-        #     subprocess.run(
-        #         ["python3", "./main.py", f"./input/ver/{benchmark}.v", "-app", f"./input/ver/{benchmark}.v", "--grid",
-        #          f"-et={et}", "--lut",
-        #          f"-spo={spo}", f"--iterations={num_iterations}", "--min_labeling", f"-num_models={num_models}"
-        #         ])
-        # 
-        #     subprocess.run(
-        #         ["python3", "./main.py", f"./input/ver/{benchmark}.v", "-app", f"./input/ver/{benchmark}.v", "--grid",
-        #           f"-et={et}", "--lut_MP",
-        #          f"-spo={spo}", f"--iterations={num_iterations}", "--min_labeling", f"-num_models={num_models}"
-        #          ])
+        for et in ET_points:
+            subprocess.run(
+                ["python3", "./main.py", f"./input/ver/{benchmark}.v", "-app", f"./input/ver/{benchmark}.v", "--grid",
+                 f"-et={et}", "--lut",
+                 f"-spo={spo}", f"--iterations={num_iterations}", "--min_labeling", f"-num_models={num_models}"
+                ])
 
-            #
-            # subprocess.run(
-            #     ["python3", "./main.py", f"./input/ver/{benchmark}.v", "-app", f"./input/ver/{benchmark}.v", "--grid",
-            #      f"-et={et}", f"--ppo={ppo}", f"--lpp={lpp}",
-            #      f"--iterations={num_iterations}", "--min_labeling", f"-num_models={num_models}"])
+            subprocess.run(
+                ["python3", "./main.py", f"./input/ver/{benchmark}.v", "-app", f"./input/ver/{benchmark}.v", "--grid",
+                  f"-et={et}", "--lut_MP",
+                 f"-spo={spo}", f"--iterations={num_iterations}", "--min_labeling", f"-num_models={num_models}"
+                 ])
+
+
+            subprocess.run(
+                ["python3", "./main.py", f"./input/ver/{benchmark}.v", "-app", f"./input/ver/{benchmark}.v", "--grid",
+                 f"-et={et}", f"--ppo={ppo}", f"--lpp={lpp}",
+                 f"--iterations={num_iterations}", "--min_labeling", f"-num_models={num_models}"])
 
     for benchmark in benchmarks:
         # best_models_area_lut = []
@@ -84,7 +82,7 @@ def run_benchmark(benchmarks=["abs_diff_i4_o2"], metric_cols=[5]):
         num_outputs = int((benchmark.split("_")[-1].split("o")[-1]))
 
         if num_outputs <= 3:
-            ET_points = [1, 2, 3, 4]
+            ET_points = [1, 2]
         else:
             total = 2 ** (num_outputs - 1)
             interval = total // num_et_points
@@ -93,22 +91,23 @@ def run_benchmark(benchmarks=["abs_diff_i4_o2"], metric_cols=[5]):
 
         for et in ET_points:
             files = get_files_matching_regex(f"spo_array.*{benchmark}.*et{et}.*LUT")
-            best_area_for_et_lut = get_best_metric_for_et(files, metric_cols, et)
+            best_area_for_et_lut = get_best_metric_for_et(files, metric_cols, scatter_plot=scatter_plots, sum_all=True)
             if best_area_for_et_lut is not None:
                 best_area_per_et_lut[et] = best_area_for_et_lut
 
             files = get_files_matching_regex(f"grid.*{benchmark}.*et{et}.*SOP1")
-            best_area_for_et_sop = get_best_metric_for_et(files, metric_cols, et)
+            best_area_for_et_sop = get_best_metric_for_et(files, metric_cols,scatter_plot=scatter_plots, sum_all=True)
             if best_area_for_et_sop is not None:
                 best_area_per_et_sop[et] = best_area_for_et_sop
 
             files = get_files_matching_regex(f"spo_array.*{benchmark}.*et{et}.*LUTMP")
-            best_area_for_et_lut_MP = get_best_metric_for_et(files, metric_cols, et)
+            best_area_for_et_lut_MP = get_best_metric_for_et(files, metric_cols, scatter_plot=scatter_plots, sum_all=True)
             if best_area_for_et_lut_MP is not None:
                 best_area_per_et_lut_MP[et] = best_area_for_et_lut_MP
 
-        plot_results([best_area_per_et_lut, best_area_per_et_lut_MP], ("LUT","LUT MP"),
-                     benchmark, [x_label_name,y_label_name])
+        plot_results([best_area_per_et_lut, best_area_per_et_lut_MP, best_area_per_et_sop], ("LUT","LUT MP","SOP"),
+                     benchmark, [x_label_name,y_label_name],scatter_plot=scatter_plots)
+
 
 
 def get_files_matching_regex(regex: str) -> List[str]:
@@ -118,63 +117,73 @@ def get_files_matching_regex(regex: str) -> List[str]:
     return files
 
 
-def get_best_metric_for_et(files: List[str], metric_cols: List[int], et:int) -> Union[List[str],None]:
+def get_best_metric_for_et(files: List[str], metric_cols: List[int], scatter_plot: bool, sum_all: bool) -> Union[List[str],float,None]:
     metric_for_et = []
+    sum_rows = 0
+    include_result = False
     for file in files:
         file = f"{z3logpath.OUTPUT_PATH['report'][0]}" + "/" + file
         with open(file, newline='') as f:
             reader = csv.reader(f)
-            # et_file = re.findall("et\d", file)[0].split("et")[1]
+
             for row in reader:
                 if reader.line_num == 1:
                     continue
 
+                if sum_all and len(metric_cols) > 1:
+                    sum_rows += sum([float(row[i]) for i in metric_cols])
+
                 if row[3] == "SAT":
-                    metric_for_et.append(sum([float(row[i]) for i in metric_cols]))
-    if metric_for_et:
-        # print(min(metric_for_et, key = metric_for_et.get))
-        # print(min(list(metric_for_et.values())))
-        return min(metric_for_et)
+                    if len(metric_cols) > 1:
+                        include_result = True
+                        sum_rows += sum([float(row[i]) for i in metric_cols])
+                    else:
+                        metric_for_et.append(row[metric_cols[0]])
+    if metric_for_et and len(metric_cols) == 1:
+        if scatter_plot:
+            return metric_for_et
+        else:
+            return min(metric_for_et)
+    elif len(metric_cols) > 1 and include_result:
+        return sum_rows
     else:
         return None
 
 
-def plot_results(results, legend, benchmark_name, labels):
+def plot_results(results, legend, benchmark_name, labels, scatter_plot = False):
 
-    fig,axs = plt.subplots()
-    # axs_idx = 0
-    for result in results:
-        x = list(result.keys())
-        y = list(result.values())
+    if scatter_plot:
 
-        y = [float(i) for i in y]
-        x = [float(i) for i in x]
+        for result in results:
+            final_x = []
+            final_y = []
+            x = list(result.keys())
+            for et in x:
+                y = list(result[et])
+                final_y += y
+                final_x += [et] * len(y)
+            plt.scatter(final_x,final_y, alpha=0.1)
+        plt.title(benchmark_name)
 
-        axs.plot(x, y, alpha =0.7, linestyle='dashed', marker='o')
+    else:
+        fig,axs = plt.subplots()
+        # axs_idx = 0
+        for result in results:
+            x = list(result.keys())
+            y = list(result.values())
+
+            y = [float(i) for i in y]
+            x = [float(i) for i in x]
+
+            axs.plot(x, y, alpha =0.7, linestyle='dashed', marker='o')
+        fig.suptitle(benchmark_name)
 
     plt.xlabel(labels[0])
     plt.ylabel(labels[1])
     plt.gca().legend(legend)
-    fig.suptitle(benchmark_name)
-    #
-    # plt.set_xticks(np.arange(1,num_et_points,1))
-    # plt.set_yticks(np.arange(0,20,1))
-    # ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
-    #        ylim=(0, 8), yticks=np.arange(1, 8))
 
     plt.show()
-    # figure_name = f"{benchmark_name}"
-    # plt.savefig(figure_name + ".png")
+    figure_name = f"{benchmark_name}"
+    plt.savefig(figure_name + ".png")
 
-
-
-
-    #
-    # header = list(range(len(cur_model_results)))
-    # all = list(cur_model_results.values())
-    # content = [f for (f, _, _, _) in all]
-    # print(f'{content = }')
-    #
-    # csvwriter.writerow(header)
-    # csvwriter.writerow(content)
-run_benchmark(metric_cols=[5])
+run_benchmark(benchmarks=["abs_diff_i4_o2","abs_diff_i6_o3","adder_i4_o3","adder_i6_o4","mul_i4_o4","mul_i6_o6"], metric_cols=[5], scatter_plots=False)
