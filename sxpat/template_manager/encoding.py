640271c5f25ffce4da06d9649e723b8c886e0f05
MarcusTral <biasion.marco.2001@gmail.com>
2024-06-17 18:16:39 +0200

    #56: cleanup and minor changes in encoding modules

--- sxpat/template_manager/encoding.py
+++ sxpat/template_manager/encoding.py
@@ -9,24 +9,16 @@ class Encoding:
     """
 
     def __init__(self,
-                 inputs_count: int, 
+                 inputs_count: int,
                  outputs_count: int,
-                 # full_inputs: Collection[str], sub_inputs: Collection[str],
-                 # full_outputs: Collection[str], sub_outputs: Collection[str]
                  ) -> None:
         self._inputs_count = inputs_count
         self._outputs_count = outputs_count
-        # self._full_inputs = full_inputs
-        # self._sub_inputs = sub_inputs
-        # self._full_outputs = full_outputs
-        # self._sub_outputs = sub_outputs
 
     @staticmethod
     def factory(encoding_id: int,
                 inputs_count: int,
                 outputs_count: int,
-                # full_inputs: Collection[str], sub_inputs: Collection[str],
-                # full_outputs: Collection[str], sub_outputs: Collection[str]
                 ) -> Encoding:
         # select and return Encoding object
         return {
@@ -74,20 +66,12 @@ class Encoding:
         raise NotImplementedError(f'{self.__class__.__name__}.output_variable(...) is abstract.')
 
     @abstractmethod
-    def aggregate_variables(self, vars: Collection[str]) -> str:
+    def aggregate_variables(self, vars: Collection[str], in_new_line: bool = False) -> str:
         raise NotImplementedError(f'{self.__class__.__name__}.aggregate_variables(...) is abstract.')
 
     def function(self, name: str) -> str:
         inputs = ', '.join(['BoolSort()'] * self._inputs_count)
         return f"Function('{name}', {inputs}, {self.sort})"
-
-    # @abstractmethod
-    # def declare_gate_step1(self, name: str) -> str:
-    #     raise NotImplementedError(f'{self.__class__.__name__}.declare_gate_step1(...) is abstract.')
-
-    # @abstractmethod
-    # def declare_gate_step2(self, name: str) -> str:
-    #     raise NotImplementedError(f'{self.__class__.__name__}.declare_gate_step1(...) is abstract.')
 
 
 class IntegerEncoding(Encoding):
@@ -122,10 +106,11 @@ class IntegerEncoding(Encoding):
         return f'IntVal({value})'
 
     def output_variable(self, name: str) -> str:
-        return f'Int({name})'
+        return f"Int('{name}')"
 
-    def aggregate_variables(self, vars: Collection[str]) -> str:
-        return ' + '.join(
+    def aggregate_variables(self, vars: Collection[str], in_new_line: bool = False) -> str:
+        sep = {True: '\n+ ', False: ' + '}[in_new_line]
+        return sep.join(
             f'If({var}, {2**i}, 0)'
             for i, var in enumerate(vars)
         )
@@ -171,48 +156,10 @@ class BitVectorEncoding(Encoding):
     def output_variable(self, name: str) -> str:
         return self._variable(name, self._outputs_count)
 
-    def aggregate_variables(self, vars: Collection[str]) -> str:
+    def aggregate_variables(self, vars: Collection[str], in_new_line: bool = False) -> str:
         bits_count = len(vars)
-        return ' + '.join(
+        sep = {True: '\n+ ', False: ' + '}[in_new_line]
+        return sep.join(
             f'If({var}, {self._value(2 ** i, bits_count)}, {self._value(0, bits_count)})'
             for i, var in enumerate(vars)
         )
-
-
-# class FunctionEncoding(Encoding):
-#     """
-#     Abstract Encoding: defining behavioural operations
-#     """
-
-#     def _use(self, name: str) -> str:
-#         if name in self._full_inputs:
-#             return name
-
-#         inputs = self._sub_inputs if name in self._sub_outputs else self._full_inputs
-#         args = ', '.join(self._use(g) for g in inputs)
-#         return f'{name}({args})'
-
-#     def declare_gate_step1(self, name: str) -> str:
-#         count = 1 + len(self._sub_inputs if name in self._sub_outputs else self._full_inputs)
-#         bool_refs = ', '.join(['BoolSort()'] * count)
-#         return f"{name} = Function('name', {bool_refs})"
-
-#     @abstractmethod
-#     def declare_gate_step2(self, name: str) -> str:
-#         return f'{self._use(name)} == '
-#         raise NotImplementedError(f'{self.__class__.__name__}.declare_gate_step1(...) is abstract.')
-
-
-# class DirectEncoding(Encoding):
-#     """
-#     Abstract Encoding: defining behavioural operations
-#     """
-
-#     def declare_gate_step1(self, name: str) -> str:
-#         # count = 1 + len(self._sub_inputs if name in self._sub_outputs else self._full_inputs)
-#         # bool_refs = ', '.join(['BoolSort()'] * count)
-#         return f"{name} = Function('name', {bool_refs})"
-
-#     @abstractmethod
-#     def declare_gate_step2(self, name: str) -> str:
-#         raise NotImplementedError(f'{self.__class__.__name__}.declare_gate_step1(...) is abstract.')
