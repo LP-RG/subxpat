15a64910707f1e06692ae42f777d586eaaba1ffd
MarcusTral <biasion.marco.2001@gmail.com>
2024-06-17 13:28:58 +0200

    #56: minor changes to the base template text

--- sxpat/template_manager/template.py
+++ sxpat/template_manager/template.py
@@ -5,11 +5,11 @@ from typing import Tuple, List, Callable, Any, Union
 import json
 import csv
 
-ET = int(sys.argv[1])
-{{{{ET_ENC}}}}
+et = int(sys.argv[1])
+et_encoded = {{{{et_encoded}}}}
 wanted_models: int = 1 if len(sys.argv) < 3 else int(sys.argv[2])
 timeout: float = float(sys.maxsize if len(sys.argv) < 4 else sys.argv[3])
-max_possible_ET: int = 2 ** 3 - 1
+max_possible_et: int = 2 ** 3 - 1
 
 # Encoding abs_diff method to generate
 def z3_abs_diff(a, b):
@@ -38,7 +38,7 @@ error = {{{{error}}}}
 {{{{exact_wires_declaration}}}}
 
 # wires functions declaration for approximate circuit
-# ei = Function('ei', BoolSort(), BoolSort(), BoolSort(), BoolSort(), BoolSort(), BoolSort(), BoolSort(), BoolSort(), BoolSort())
+# ai = Function('ai', BoolSort(), BoolSort(), BoolSort(), BoolSort(), BoolSort(), BoolSort(), BoolSort(), BoolSort(), BoolSort())
 {{{{approximate_wires_declaration}}}}
 
 # outputs bitvectors declaration for exact circuit
@@ -52,36 +52,27 @@ error = {{{{error}}}}
 # exact circuit constraints
 exact_circuit = And(
 	# wires
-{{{{exact_wires_constraints}}}}
+	{{{{exact_wires_constraints}}}}
 	
 	# output bits (from the least significant)
-{{{{exact_output_constraints}}}}
+	{{{{exact_output_constraints}}}}
 	
 	# aggregated output
-{{{{exact_aggregated_output}}}}
+	{{{{exact_aggregated_output}}}}
 )
 
 # approximate circuit constraints
 approximate_circuit = And(
 	# wires
-{{{{approximate_wires_constraints}}}}
+	{{{{approximate_wires_constraints}}}}
 
 	# output bits (from the least significant)
-{{{{approximate_output_constraints}}}}
+	{{{{approximate_output_constraints}}}}
 	
 	# aggregated output
-{{{{approximate_aggregated_output}}}}
+	{{{{approximate_aggregated_output}}}}
 )
 
-
-# boolean outputs (from the least significant) as bitvectors
-# aouti(ini) == If(a32(in0,in4,a20(in0, in1, in2, in3, in4, in5, in6, in7),a26(in0, in1, in2, in3, in4, in5, in6, in7),a31(in0, in1, in2, in3, in4, in5, in6, in7),a50(in0, in1, in2, in3, in4, in5, in6, in7),a53(in0, in1, in2, in3, in4, in5, in6, in7)), BitVecVal(0b00001, 5), BitVecVal(0b00000, 5)),
-{{{{boolean_outputs}}}}
-
-# approximate_bitvec_outputs
-# fa(ini) == 
-# aouti(ini) + ...
-{{{{approximate_outputs}}}}
 
 # forall and verification solvers
 forall_solver = {{{{solver}}}}
@@ -89,43 +80,31 @@ forall_solver.add(ForAll(
 	[{{}}],
 	And(
 		# error constraints
-		{{{{difference_less_equal_et}}}},
+		{{{{difference_less_equal_etenc}}}},
 
 		# circuits
 		exact_circuit,
 		approximate_circuit,
 
+		{{{{logic_dependant_constraint1}}}}
+		
+		#> redundancy constraints
+
+		# remove double no-care
+		{{{{remove_double_constraint}}}}
+		Implies(p_o0_t0_i0_l, p_o0_t0_i0_s), Implies(p_o0_t0_i1_l, p_o0_t0_i1_s), Implies(p_o0_t0_i2_l, p_o0_t0_i2_s), Implies(p_o0_t0_i3_l, p_o0_t0_i3_s), Implies(p_o0_t0_i4_l, p_o0_t0_i4_s), Implies(p_o0_t0_i5_l, p_o0_t0_i5_s), Implies(p_o0_t0_i6_l, p_o0_t0_i6_s), Implies(p_o0_t1_i0_l, p_o0_t1_i0_s), Implies(p_o0_t1_i1_l, p_o0_t1_i1_s), Implies(p_o0_t1_i2_l, p_o0_t1_i2_s), Implies(p_o0_t1_i3_l, p_o0_t1_i3_s), Implies(p_o0_t1_i4_l, p_o0_t1_i4_s), Implies(p_o0_t1_i5_l, p_o0_t1_i5_s), Implies(p_o0_t1_i6_l, p_o0_t1_i6_s), Implies(p_o0_t2_i0_l, p_o0_t2_i0_s), Implies(p_o0_t2_i1_l, p_o0_t2_i1_s), Implies(p_o0_t2_i2_l, p_o0_t2_i2_s), Implies(p_o0_t2_i3_l, p_o0_t2_i3_s), Implies(p_o0_t2_i4_l, p_o0_t2_i4_s), Implies(p_o0_t2_i5_l, p_o0_t2_i5_s), Implies(p_o0_t2_i6_l, p_o0_t2_i6_s), 
+		Implies(p_o1_t0_i0_l, p_o1_t0_i0_s), Implies(p_o1_t0_i1_l, p_o1_t0_i1_s), Implies(p_o1_t0_i2_l, p_o1_t0_i2_s), Implies(p_o1_t0_i3_l, p_o1_t0_i3_s), Implies(p_o1_t0_i4_l, p_o1_t0_i4_s), Implies(p_o1_t0_i5_l, p_o1_t0_i5_s), Implies(p_o1_t0_i6_l, p_o1_t0_i6_s), Implies(p_o1_t1_i0_l, p_o1_t1_i0_s), Implies(p_o1_t1_i1_l, p_o1_t1_i1_s), Implies(p_o1_t1_i2_l, p_o1_t1_i2_s), Implies(p_o1_t1_i3_l, p_o1_t1_i3_s), Implies(p_o1_t1_i4_l, p_o1_t1_i4_s), Implies(p_o1_t1_i5_l, p_o1_t1_i5_s), Implies(p_o1_t1_i6_l, p_o1_t1_i6_s), Implies(p_o1_t2_i0_l, p_o1_t2_i0_s), Implies(p_o1_t2_i1_l, p_o1_t2_i1_s), Implies(p_o1_t2_i2_l, p_o1_t2_i2_s), Implies(p_o1_t2_i3_l, p_o1_t2_i3_s), Implies(p_o1_t2_i4_l, p_o1_t2_i4_s), Implies(p_o1_t2_i5_l, p_o1_t2_i5_s), Implies(p_o1_t2_i6_l, p_o1_t2_i6_s), 
+
+		# remove constant 0 parameters permutations
+		{{{{remove_zero_permutations_constraint}}}}
+		Implies(Not(p_o0), Not(Or(p_o0_t0_i0_s, p_o0_t0_i0_l, p_o0_t0_i1_s, p_o0_t0_i1_l, p_o0_t0_i2_s, p_o0_t0_i2_l, p_o0_t0_i3_s, p_o0_t0_i3_l, p_o0_t0_i4_s, p_o0_t0_i4_l, p_o0_t0_i5_s, p_o0_t0_i5_l, p_o0_t0_i6_s, p_o0_t0_i6_l, p_o0_t1_i0_s, p_o0_t1_i0_l, p_o0_t1_i1_s, p_o0_t1_i1_l, p_o0_t1_i2_s, p_o0_t1_i2_l, p_o0_t1_i3_s, p_o0_t1_i3_l, p_o0_t1_i4_s, p_o0_t1_i4_l, p_o0_t1_i5_s, p_o0_t1_i5_l, p_o0_t1_i6_s, p_o0_t1_i6_l, p_o0_t2_i0_s, p_o0_t2_i0_l, p_o0_t2_i1_s, p_o0_t2_i1_l, p_o0_t2_i2_s, p_o0_t2_i2_l, p_o0_t2_i3_s, p_o0_t2_i3_l, p_o0_t2_i4_s, p_o0_t2_i4_l, p_o0_t2_i5_s, p_o0_t2_i5_l, p_o0_t2_i6_s, p_o0_t2_i6_l))),
+		Implies(Not(p_o1), Not(Or(p_o1_t0_i0_s, p_o1_t0_i0_l, p_o1_t0_i1_s, p_o1_t0_i1_l, p_o1_t0_i2_s, p_o1_t0_i2_l, p_o1_t0_i3_s, p_o1_t0_i3_l, p_o1_t0_i4_s, p_o1_t0_i4_l, p_o1_t0_i5_s, p_o1_t0_i5_l, p_o1_t0_i6_s, p_o1_t0_i6_l, p_o1_t1_i0_s, p_o1_t1_i0_l, p_o1_t1_i1_s, p_o1_t1_i1_l, p_o1_t1_i2_s, p_o1_t1_i2_l, p_o1_t1_i3_s, p_o1_t1_i3_l, p_o1_t1_i4_s, p_o1_t1_i4_l, p_o1_t1_i5_s, p_o1_t1_i5_l, p_o1_t1_i6_s, p_o1_t1_i6_l, p_o1_t2_i0_s, p_o1_t2_i0_l, p_o1_t2_i1_s, p_o1_t2_i1_l, p_o1_t2_i2_s, p_o1_t2_i2_l, p_o1_t2_i3_s, p_o1_t2_i3_l, p_o1_t2_i4_s, p_o1_t2_i4_l, p_o1_t2_i5_s, p_o1_t2_i5_l, p_o1_t2_i6_s, p_o1_t2_i6_l))),
+
+		# set order of products
+		{{{{product_order_constraint}}}}
 	)
 ))
-# forall_solver.add(ForAll(
-# 	[in0,in1,in2,in3,in4,in5,in6,in7],
-# 	And(
 
-# 		# error constraints
-# 		difference <= ET,
-
-# 		# circuits
-# 		exact_circuit,
-# 		approximate_circuit,
-
-# 		# Force the number of inputs to sums to be at most as its
-# 		(IntVal(1) * p_pr0_o0 + IntVal(1) * p_pr1_o0 + IntVal(1) * p_pr2_o0 + IntVal(1) * p_pr0_o1 + IntVal(1) * p_pr1_o1 + IntVal(1) * p_pr2_o1) <= 3,
-
-# 		# Redundancy constraints
-# 		# remove double no-care
-# 		Implies(p_pr0_i0_l, p_pr0_i0_s), Implies(p_pr0_i1_l, p_pr0_i1_s), Implies(p_pr0_i2_l, p_pr0_i2_s), Implies(p_pr0_i3_l, p_pr0_i3_s), Implies(p_pr0_i4_l, p_pr0_i4_s), Implies(p_pr0_i5_l, p_pr0_i5_s), Implies(p_pr0_i6_l, p_pr0_i6_s), 
-# 		Implies(p_pr1_i0_l, p_pr1_i0_s), Implies(p_pr1_i1_l, p_pr1_i1_s), Implies(p_pr1_i2_l, p_pr1_i2_s), Implies(p_pr1_i3_l, p_pr1_i3_s), Implies(p_pr1_i4_l, p_pr1_i4_s), Implies(p_pr1_i5_l, p_pr1_i5_s), Implies(p_pr1_i6_l, p_pr1_i6_s), 
-# 		Implies(p_pr2_i0_l, p_pr2_i0_s), Implies(p_pr2_i1_l, p_pr2_i1_s), Implies(p_pr2_i2_l, p_pr2_i2_s), Implies(p_pr2_i3_l, p_pr2_i3_s), Implies(p_pr2_i4_l, p_pr2_i4_s), Implies(p_pr2_i5_l, p_pr2_i5_s), Implies(p_pr2_i6_l, p_pr2_i6_s), 
-
-# 		# remove constant 0 parameters permutations
-# 		Implies(Not(p_o0), Not(Or(p_pr0_o0, p_pr1_o0, p_pr2_o0))), 
-# 		Implies(Not(p_o1), Not(Or(p_pr0_o1, p_pr1_o1, p_pr2_o1))), 
-
-# 		# set order of pits
-# 		(IntVal(1) * p_pr0_i0_s + IntVal(2) * p_pr0_i0_l + IntVal(4) * p_pr0_i1_s + IntVal(8) * p_pr0_i1_l + IntVal(16) * p_pr0_i2_s + IntVal(32) * p_pr0_i2_l + IntVal(64) * p_pr0_i3_s + IntVal(128) * p_pr0_i3_l + IntVal(256) * p_pr0_i4_s + IntVal(512) * p_pr0_i4_l + IntVal(1024) * p_pr0_i5_s + IntVal(2048) * p_pr0_i5_l + IntVal(4096) * p_pr0_i6_s + IntVal(8192) * p_pr0_i6_l) > (IntVal(1) * p_pr1_i0_s + IntVal(2) * p_pr1_i0_l + IntVal(4) * p_pr1_i1_s + IntVal(8) * p_pr1_i1_l + IntVal(16) * p_pr1_i2_s + IntVal(32) * p_pr1_i2_l + IntVal(64) * p_pr1_i3_s + IntVal(128) * p_pr1_i3_l + IntVal(256) * p_pr1_i4_s + IntVal(512) * p_pr1_i4_l + IntVal(1024) * p_pr1_i5_s + IntVal(2048) * p_pr1_i5_l + IntVal(4096) * p_pr1_i6_s + IntVal(8192) * p_pr1_i6_l), 
-# 		(IntVal(1) * p_pr1_i0_s + IntVal(2) * p_pr1_i0_l + IntVal(4) * p_pr1_i1_s + IntVal(8) * p_pr1_i1_l + IntVal(16) * p_pr1_i2_s + IntVal(32) * p_pr1_i2_l + IntVal(64) * p_pr1_i3_s + IntVal(128) * p_pr1_i3_l + IntVal(256) * p_pr1_i4_s + IntVal(512) * p_pr1_i4_l + IntVal(1024) * p_pr1_i5_s + IntVal(2048) * p_pr1_i5_l + IntVal(4096) * p_pr1_i6_s + IntVal(8192) * p_pr1_i6_l) > (IntVal(1) * p_pr2_i0_s + IntVal(2) * p_pr2_i0_l + IntVal(4) * p_pr2_i1_s + IntVal(8) * p_pr2_i1_l + IntVal(16) * p_pr2_i2_s + IntVal(32) * p_pr2_i2_l + IntVal(64) * p_pr2_i3_s + IntVal(128) * p_pr2_i3_l + IntVal(256) * p_pr2_i4_s + IntVal(512) * p_pr2_i4_l + IntVal(1024) * p_pr2_i5_s + IntVal(2048) * p_pr2_i5_l + IntVal(4096) * p_pr2_i6_s + IntVal(8192) * p_pr2_i6_l), 
-# 	)
-# ))
 verification_solver = {{{{solver}}}}
 verification_solver.add(
 	error == difference,
@@ -164,7 +143,7 @@ while(len(found_data) < wanted_models and timeout > 0):
 				parameters_constraints.append((Bool(str(k)), v))
 		# verify parameters
 		WCE: int = None
-		verification_ET: int = 0
+		verification_et: int = 0
 		time_verification_start = time()
 		# save state
 		verification_solver.push()
@@ -173,27 +152,27 @@ while(len(found_data) < wanted_models and timeout > 0):
 			*map(lambda x: x[0] == x[1], parameters_constraints),
 		)
 
-		while verification_ET < max_possible_ET:
+		while verification_et < max_possible_et:
 			# add constraint (difference > verification_ET) or UGE(...)
-			verification_solver.add({{{{difference_greater_verET}}}})
+			verification_solver.add({{{{difference_greater_veret}}}})
 			# run solver
 			verification_solver.set("timeout", int(timeout * 1000))
 			v_result = verification_solver.check()
 			if v_result == unsat:
 				# unsat, WCE found
-				WCE = verification_ET
+				WCE = verification_et
 				break
 			elif v_result == sat:
 				# sat, need to search again
 				m = verification_solver.model()
-				verification_ET = m[error].as_long()
+				verification_et = m[error].as_long()
 			else:
 				 # unknown (probably a timeout)
 				WCE = -1
 				break
 
 		if WCE is None:
-			WCE = max_possible_ET
+			WCE = max_possible_et
 		# revert state
 		verification_solver.pop()
 		time_verification = time() - time_verification_start
@@ -202,7 +181,7 @@ while(len(found_data) < wanted_models and timeout > 0):
 		attempts_times.append((time_attempt, time_parameters, time_verification))
 		
 		# ==== continue or exit
-		if WCE > ET:
+		if WCE > et:
 			# Z3 hates us and decided it doesn't like being appreciated
 			result = None
 			attempts += 1
@@ -232,7 +211,15 @@ while(len(found_data) < wanted_models and timeout > 0):
 				+ typ)
 
 	time_total = time() - time_total_start
-	data_object = {{{{data_object}}}}
+	data_object = {
+		'benchmark_name': '{{{{benchmark_name}}}}',
+		'encoding': '{{{{encoding}}}}',
+		'cell': '{{{{cell}}}}',
+		'result': str(result),
+		'total_time': time_total,
+		'attempts': attempts,
+		'attempts_times': [list(map(lambda tup: [*tup], attempts_times))]
+	}
 	if result == sat:
 		data_object['model'] = dict(map(lambda item: (str(item[0]), is_true(item[1])),
 			sorted(parameters_constraints,
