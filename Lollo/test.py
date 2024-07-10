40d34520019d918a7d607b09cba3670d853605c3
lollospadalaser <spadalorenzo04@gmail.com>
2024-07-10 17:00:36 +0200

    fixed (I hope) some errors

--- Lollo/test.py
+++ Lollo/test.py
@@ -1,8 +1,7 @@
 import sys
 from collections import deque
 sys.path.append("/home/lorenzospada/Documents/SubXPAT")
-#sys.path.append("/home/lorenzospada/Documents/SubXPAT/input")
-print(sys.path)
+
 
 from sxpat.annotatedGraph import *
 
@@ -15,12 +14,12 @@ OR_SUBXPAT = 'or'
 INPUT_GATE_INITIALS = 'in'
 STANDARD_GATE_INITIALS = 'g'
 OUTPUT_GATE_INITIALS = 'out'
-CHANGE = {INPUT_GATE_INITIALS : "1", STANDARD_GATE_INITIALS : "2", OUTPUT_GATE_INITIALS : "3"} #
+CHANGE = {INPUT_GATE_INITIALS : "1", STANDARD_GATE_INITIALS : "2", OUTPUT_GATE_INITIALS : "30"} #
 
 def make_qcir_variable(var):
     for key,value in CHANGE.items():
         if var[:len(key)] == key:
-            return value + var[len(key):]
+            return value + var[len(key):] 
     raise TypeError("received a variable that I can't convert, the variable was: " + var)
 
 #specs_obj: TemplateSpecs
@@ -29,17 +28,21 @@ def check_sat(specs_obj: TemplateSpecs):
     annotated.extract_subgraph(specs_obj)
     graph = annotated.graph
     nodes = graph.nodes
+    print(annotated.subgraph_input_dict.values())
     print(annotated.subgraph_output_dict.values())
-    #print(annotated.subgraph_num_inputs, annotated.subgraph_num_outputs, specs_obj.ppo)
-    #print(annotated.graph.nodes)
-    print(annotated.graph.nodes['g113'])
-    print(annotated.graph.nodes['g114'])
-    #print(*annotated.graph.neighbors('g20'))
-    #print(*annotated.graph.successors('g20'))
-    #print(list(annotated.graph.predecessors('out0')))
+    # print(specs_obj.et)
+    # print(specs_obj.max_ppo)
+    # print(annotated.subgraph_num_inputs, annotated.subgraph_num_outputs, specs_obj.max_ppo)
+    # print(annotated.graph.nodes)
+    # print(annotated.graph.nodes['g106'])
+    # print(*annotated.graph.neighbors('g20'))
+    print(*annotated.graph.predecessors('g18'))
+    print(*annotated.graph.predecessors('g35'))
+    #print(list(annotated.graph.predecessors('g103')))
     
-    # 1,2,3 is for the input, and, output gates of the exact circuit, 40 for intermidiate and gates of the multiplexer, 41 for the output of the multiplexer,
-    # 5 for the and gates, 60 for the or gates, 61 for the and between the or gate and p_o# (the outputs of the parametrical circuit), 7 is for the parameters p_o#_t#_i#_s/l,
+    # 1,2,30 is for the input, and, output gates of the exact circuit, 40 for intermidiate and gates of the multiplexer, 41 for the output of the multiplexer,
+    # 5 for the and gates, 60 for the or gates, 61 for the and between the or gate and p_o# (the outputs of the parametrical circuit),
+    # 62 is for the and gates of the inexact circuit, 7 is for the parameters p_o#_t#_i#_s/l,
     # 90 is for the satisfability problem, 91 for false constant, 92 for true constant, 93 for the parameters p_o#
     output = open('./Lollo/output.txt','w')
     
@@ -49,11 +52,11 @@ def check_sat(specs_obj: TemplateSpecs):
     output.write('exists(')
     start = True
     for a in range(annotated.subgraph_num_outputs):
-        for t in range(specs_obj.ppo):
+        for t in range(specs_obj.max_ppo):
             for c in range(annotated.subgraph_num_inputs):
                 if not start:
                     output.write(', ')
-                var = ((a * specs_obj.ppo + t) * annotated.subgraph_num_inputs + c) * 2
+                var = ((a * specs_obj.max_ppo + t) * annotated.subgraph_num_inputs + c) * 2
                 output.write('7' + str(var) + ', 7' + str(var+1))
                 start = False
         output.write(', 93' + str(a))
@@ -131,7 +134,7 @@ def check_sat(specs_obj: TemplateSpecs):
     pres = {}  # qcir_gate_name : qcir_gate_you_come_from
     inverted = {} #key : [qcir_gate_referring_to, inverted?]
     for x in nodes:
-        if x[:len(INPUT_GATE_INITIALS)] != INPUT_GATE_INITIALS or x in annotated.subgraph_input_dict.keys():
+        if x[:len(INPUT_GATE_INITIALS)] != INPUT_GATE_INITIALS:
             continue
         for succ in graph.successors(x):
             if nodes[succ]['label'] == NOT or make_qcir_variable(succ) in pres:
@@ -142,7 +145,7 @@ def check_sat(specs_obj: TemplateSpecs):
     #quasi uguale a sopra, sistemare
     while len(deq) != 0:
         cur = deq.popleft()
-        if cur in annotated.subgraph_input_dict.keys():
+        if cur in annotated.subgraph_output_dict.values():
             continue
         label = nodes[cur]['label']
         if label == NOT:
@@ -152,22 +155,23 @@ def check_sat(specs_obj: TemplateSpecs):
                 inverted[make_qcir_variable(cur)] = [inverted[make_qcir_variable(predecessor)][0], not inverted[make_qcir_variable(predecessor)][1]]
             else:
                 inverted[make_qcir_variable(cur)] = [predecessor, True]
-
+        
+        
         for x in graph.successors(cur):
-            if nodes[x]['label'] == NOT or x in pres:
+            if nodes[x]['label'] == NOT or make_qcir_variable(x) in pres:
                 deq.append(x)
             else:
-                pres[x] = make_qcir_variable(cur)
+                pres[make_qcir_variable(x)] = make_qcir_variable(cur)
     
     #formula of multiplexer or( and(s,l,in), and(s, !l, !in), !s)
     for a,out in enumerate(annotated.subgraph_output_dict.values()):
         and_list = []
 
-        for t in range(specs_obj.ppo):
+        for t in range(specs_obj.max_ppo):
             multi_list = []
 
             for c,inp in enumerate(annotated.subgraph_input_dict.values()):
-                var = ((a * specs_obj.ppo + t) * annotated.subgraph_num_inputs + c) * 2
+                var = ((a * specs_obj.max_ppo + t) * annotated.subgraph_num_inputs + c) * 2
                 and1 = '40' + str(var)
                 and2 = '40' + str(var+1)
 
@@ -184,7 +188,7 @@ def check_sat(specs_obj: TemplateSpecs):
                 multi_list.append(multi)
 
             #and gates of the parametrical circuit
-            var = a * specs_obj.ppo + t
+            var = a * specs_obj.max_ppo + t
             andg = '5' + str(var)
             and_list.append(andg)
             output.write(andg + ' = and(')
@@ -213,9 +217,32 @@ def check_sat(specs_obj: TemplateSpecs):
         outp = '61' + str(a)
         output.write(outp + ' = and(' + p_o + ', ' + org + ')\n')
 
-    # for x in annotated.subgraph_output_dict.values():
-    #     for succ in graph.successors(x):
-    #         label = 
+    output.write('\n#continuing exploration of inexact_circuit\n')
+    #continuing exploration of inexact_circuit
+    for a,out in enumerate(annotated.subgraph_output_dict.values()):
+        for succ in graph.successors(out):
+            label = nodes[succ]['label']
+            qcir_out = '61' + str(a)
+            # print(succ,pres[make_qcir_variable(succ)])
+            if label == NOT:
+                inverted[succ] = [qcir_out,True]
+                deq.append(succ)
+            
+            
+            elif make_qcir_variable(succ) in pres:
+                deq.append(succ)
+                output.write('62' + succ[len(STANDARD_GATE_INITIALS):] + ' = and(' + pres[make_qcir_variable(succ)] + ', ' + qcir_out + ')\n')
+
+            else:
+                pres[make_qcir_variable(succ)] = qcir_out
+    
+
+            
+
+            
+            
+            
+
         
 
 
