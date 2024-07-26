cf4ddc09ecb653a7560cee6911f2a19b48d085b4
Francesco <costafr@usi.ch>
2024-07-26 10:57:37 +0200

    #56.2: Added fixes to labeling

--- sxpat/xplore.py
+++ sxpat/xplore.py
@@ -137,19 +137,11 @@ def explore_grid(specs_obj: TemplateSpecs):
         pprint.info1(f'iteration {i} with et {et}, available error {available_error}'
                      if (specs_obj.subxpat or specs_obj.subxpat_v2) else
                      f'Only one iteration with et {et}')
-
-        max_unsat_reached = False
-
+        
         # for all candidates
         # print(f'{current_population = }')
         for candidate in current_population:
-            # guard
-            # if pre_iter_unsats[candidate] == total_number_of_cells_per_iter and not specs_obj.keep_unsat_candidate:
-            #     pprint.info1(f'Number of UNSATs reached!')
-            #     max_unsat_reached = True
-            #     pre_iter_unsats[candidate] = 0
-            #     continue
-
+            
             pprint.info1(f'candidate {candidate}')
             if candidate.endswith('.v'):
                 specs_obj.benchmark_name = candidate[:-2]
@@ -266,11 +258,11 @@ def explore_grid(specs_obj: TemplateSpecs):
                     for candidate in cur_model_results:
                         approximate_benchmark = candidate[:-2]
 
-                        obtained_wce_exact = erroreval_verification_wce(exact_file_name, approximate_benchmark, available_error)
-                        obtained_wce_prev = erroreval_verification_wce(specs_obj.exact_benchmark, approximate_benchmark, available_error)
+                        obtained_wce_exact = erroreval_verification_wce(exact_file_name, approximate_benchmark, et)
+                        obtained_wce_prev = erroreval_verification_wce(specs_obj.exact_benchmark, approximate_benchmark, et)
                         prev_actual_error = obtained_wce_prev
 
-                        if obtained_wce_exact > available_error:
+                        if obtained_wce_exact > et:
                             stats_obj.store_grid()
                             return stats_obj
                             # raise Exception(color.error('ErrorEval Verification: FAILED!'))
@@ -288,7 +280,7 @@ def explore_grid(specs_obj: TemplateSpecs):
                     stats_obj.grid.cells[lpp][ppo].store_model_info(this_model_info)
                     pprint.success(f'ErrorEval PASS! with total wce = {obtained_wce_exact}')
 
-                    benchmark_name = specs_obj.benchmark_name
+                    specs_obj.benchmark_name = approximate_benchmark
 
                     synth_obj.set_path(z3logpath.OUTPUT_PATH['ver'], list(cur_model_results.keys())[0])
 
@@ -298,7 +290,7 @@ def explore_grid(specs_obj: TemplateSpecs):
                                    synth_obj.estimate_delay(exact_file_path)]
                     print_current_model(cur_model_results, normalize=False, exact_stats=exact_stats)
 
-                    store_current_model(cur_model_results, exact_stats=exact_stats, benchmark_name=benchmark_name, et=specs_obj.et,
+                    store_current_model(cur_model_results, exact_stats=exact_stats, benchmark_name=specs_obj.benchmark_name, et=specs_obj.et,
                                         encoding=specs_obj.encoding, subgraph_extraction_time=subgraph_extraction_time, labeling_time=labeling_time)
 
                     for key in cur_model_results.keys():
@@ -319,6 +311,7 @@ def explore_grid(specs_obj: TemplateSpecs):
 
                     # SAT found, stop grid exploration
                     break
+                prev_actual_error = 0
                 prev_actual_error = 0
 
         if exists_an_area_zero(current_population):
@@ -522,10 +515,10 @@ def display_the_tree(this_dict: Dict) -> None:
     #     pass
 
 
-def label_graph(exact_graph: AnnotatedGraph, current_graph: AnnotatedGraph,
+def label_graph(current_graph: AnnotatedGraph,
                 min_labeling: bool = False,  partial: bool = False,
                 et: int = -1, parallel: bool = False):
-    labels, _ = labeling_explicit(exact_graph.name, current_graph.name,
+    labels, _ = labeling_explicit(current_graph.name, current_graph.name,
                                   constant_value=0, min_labeling=min_labeling,
                                   partial=partial, et=et, parallel=parallel)
 
