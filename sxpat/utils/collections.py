f2dd73745bd7d112fd0713c02e25729bc3283373
Marco Biasion <biasion.marco.2001@gmail.com>
2024-09-09 13:39:20 +0200

    #81: implemented multiDict and InheritanceMapping utilities collections

--- sxpat/utils/collections.py
+++ sxpat/utils/collections.py
@@ -1,4 +1,6 @@
-from typing import Iterator, Mapping, Sequence, Tuple, TypeVar
+from __future__ import annotations
+from collections import UserDict
+from typing import Iterable, Iterator, Mapping, Sequence, Tuple, Type, TypeVar, Union
 
 import itertools
 
@@ -19,3 +21,28 @@ def mapping_inv(mapping: Mapping[K, V], value: V, default: K = NOTHING) -> K:
 def pairwise_iter(sequence: Sequence[T]) -> Iterator[Tuple[T, T]]:
     """iterate pair-wise (AB, BC, CD, ...)"""
     return zip(sequence, itertools.islice(sequence, 1, None))
+
+
+class MultiDict(UserDict, Mapping[K, V]):
+    def __init__(self, mapping: Mapping[Iterable[K], V] = None) -> None:
+        super().__init__()
+
+        if mapping is not None:
+            for ks, v in mapping.items():
+                self.__setitem__(ks, v)
+
+    def __setitem__(self, key: Iterable[K], value: V) -> None:
+        for k in key:
+            self.data[k] = value
+
+
+class InheritanceMapping(MultiDict[Type, V]):
+    def __init__(self, mapping: Mapping[Type, V] = None) -> None:
+        super().__init__(mapping)
+
+    def __setitem__(self, key: Type, value: V) -> None:
+        subtypes = [key]
+        for t in subtypes:
+            subtypes.extend(t.__subclasses__())
+
+        super().__setitem__(subtypes, value)
