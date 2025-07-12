fa3841bb672480cb8368811d1c992c6cbb4127aa
Marco Biasion <biasion.marco.2001@gmail.com>
2025-07-12 16:31:01 +0200

    #140: implemented utils::decorators::static_storage decorator

--- sxpat/utils/decorators.py
+++ sxpat/utils/decorators.py
@@ -1,4 +1,6 @@
-from typing import Callable, TypeVar
+from typing import Callable, TypeVar, overload
+
+import functools as ft
 
 from .functions import get_raiser
 
@@ -29,3 +31,23 @@ def make_uninstantiable(message: str) -> Callable[[T], T]:
 
 def make_utility_class(cls: T) -> T:
     return make_uninstantiable('{qualname} is a utility class and as such cannot be instantiated')(cls)
+
+
+def static_storage(with_self_arg: bool = False, /, **vars):
+    """
+        Assigns the keyword arguments to the function object, allowing persistent storage for these variables and values.
+
+        If `with_self_arg` is `True`, the first argument of the decorated function must be reserved for the function object, which will be used by the decorator to pass a reference to the function.
+        To improve the typing of the decorated function, `typing.overload` helps in hiding the reserved first argument.
+    """
+
+    def decorate(func):
+        # assign `vars` to the function object
+        for (k, v) in vars.items(): setattr(func, k, v)
+
+        if with_self_arg:
+            return ft.partial(func, func)
+        else:
+            return func
+
+    return decorate
