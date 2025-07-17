d9721be815c25860d3f2c49f1e61115cdd32bbd4
Marco Biasion <biasion.marco.2001@gmail.com>
2025-07-17 23:37:51 +0200

    #140: refactored distance Abso...WeightedSum to modularize individual value creation and allow for individual value to be publicly created

--- sxpat/definitions/distances/AbsoluteDifferenceOfWeightedSum.py
+++ sxpat/definitions/distances/AbsoluteDifferenceOfWeightedSum.py
@@ -1,13 +1,14 @@
-from typing import Sequence, Tuple
+from typing import Collection, Iterable, Optional, Sequence, Tuple, Union
 from typing_extensions import override
 
+import itertools as it
 from sxpat.utils.collections import first, formatted_int_range
 
 from .DistanceSpecification import DistanceSpecification
 
 from sxpat.graph import CGraph, IOGraph
 from sxpat.graph import error as g_error
-from sxpat.graph.node import AbsDiff, Extras, If, IntConstant, PlaceHolder, Sum
+from sxpat.graph.node import AbsDiff, AnyExtras, Extras, If, IntConstant, PlaceHolder, Sum
 
 
 __all__ = ['AbsoluteDifferenceOfWeightedSum']
@@ -23,12 +24,12 @@ class AbsoluteDifferenceOfWeightedSum(DistanceSpecification):
     @override
     @classmethod
     def _define(cls, graph_a: IOGraph, graph_b: IOGraph,
-                wanted_a: Sequence[str], wanted_b: Sequence[str],
+                wanted_a: Collection[str], wanted_b: Collection[str],
                 ) -> Tuple[CGraph, str]:
 
         # useful variables
-        w_nodes_a: Sequence[Extras] = tuple(graph_a[n] for n in wanted_a)  # type: ignore
-        w_nodes_b: Sequence[Extras] = tuple(graph_b[n] for n in wanted_b)  # type: ignore
+        w_nodes_a: Collection[Extras] = tuple(graph_a[n] for n in wanted_a)  # type: ignore
+        w_nodes_b: Collection[Extras] = tuple(graph_b[n] for n in wanted_b)  # type: ignore
 
         # guard
         if (broken := first(Extras.has_weight, w_nodes_a, None)) is not None:
@@ -36,52 +37,76 @@ class AbsoluteDifferenceOfWeightedSum(DistanceSpecification):
         elif (broken := first(Extras.has_weight, w_nodes_b, None)) is not None:
             raise g_error.MissingAttributeInNodeError(f'{broken} in graph_b ({graph_b}) has no weight.')
 
-        # graph_a int
-        consts_a = []
-        bits_a = []
-        for (i, node) in zip(
-            formatted_int_range(len(wanted_a)),
-            w_nodes_a,
-        ):
-            # create constants
-            val: int = node.weight  # type: ignore
-            consts_a.extend([
-                const_0 := IntConstant(f'dist_a{i}_const_0', 0),
-                const_n := IntConstant(f'dist_a{i}_const_{val}', val),
-            ])
-
-            # create node that reflects the weight if the bit is true, or 0
-            bits_a.append(If(f'dist_a{i}', operands=[node, const_n, const_0]))
-        int_a = Sum('dist_int_a', operands=bits_a)
-
-        # graph_b int
-        consts_b = []
-        bits_b = []
-        for (i, node) in zip(
-            formatted_int_range(len(wanted_b)),
-            w_nodes_b,
-        ):
-            # create constants
-            val: int = node.weight  # type: ignore
-            consts_b.extend([
-                const_0 := IntConstant(f'dist_b{i}_const_0', 0),
-                const_n := IntConstant(f'dist_b{i}_const_{val}', val),
-            ])
-
-            # create node that reflects the weight if the bit is true, or 0
-            bits_b.append(If(f'dist_b{i}', operands=[node, const_n, const_0]))
-        int_b = Sum('dist_int_b', operands=bits_b)
+        # values
+        (a_nodes, a_name) = cls._define_value(w_nodes_a, 'a')
+        (b_nodes, b_name) = cls._define_value(w_nodes_b, 'b')
 
         # distance
-        distance = AbsDiff('dist_distance', operands=[int_a, int_b])
+        distance = AbsDiff('dist_distance', operands=[a_name, b_name])
 
         # construct CGraph
-        dist_func = CGraph((
-            *(PlaceHolder(name) for name in wanted_a),
-            *consts_a, *bits_a, int_a,
-            *(PlaceHolder(name) for name in wanted_b),
-            *consts_b, *bits_b, int_b,
-            distance,
+        dist_func = CGraph(it.chain(
+            a_nodes,
+            b_nodes,
+            [distance],
         ))
 
         return (dist_func, distance.name)
+
+    @classmethod
+    def define_value(cls, graph: IOGraph, wanted: Optional[Collection[str]] = None
+                     ) -> Tuple[CGraph, str]:
+        """
+            Define an individual weighted sum given the graph and an optional specific collection of nodes to use.
+
+            @notes: if it becomes standard to generate simple expressions (eg. a weighted sum) could make sense to move this logic (and all others like this one) to their own module
+        """
+
+        # default
+        if wanted is None: wanted = graph.outputs_names
+
+        # useful variables
+        w_nodes: Sequence[Extras] = tuple(graph[n] for n in wanted)  # type: ignore
+
+        # guard
+        if (broken := first(Extras.has_weight, w_nodes, None)) is not None:
+            raise g_error.MissingAttributeInNodeError(f'{broken} in graph ({graph}) has no weight.')
+
+        (value_nodes, value_name) = cls._define_value(w_nodes, 'v')
+
+        # construct CGraph
+        value_func = CGraph(value_nodes)
+
+        return (value_func, value_name)
+
+    @classmethod
+    def _define_value(cls, wanted_nodes: Sequence[AnyExtras], value_id: str
+                      ) -> Tuple[Iterable[Union[PlaceHolder, IntConstant, If, Sum]], str]:
+
+        # graph int
+        consts_n = []
+        bits_n = []
+        for (i, node) in zip(
+            formatted_int_range(len(wanted_nodes)),
+            wanted_nodes,
+        ):
+            # create constants
+            val: int = node.weight  # type: ignore
+            consts_n.extend([
+                const_0 := IntConstant(f'dist_{value_id}{i}_const_0', 0),
+                const_n := IntConstant(f'dist_{value_id}{i}_const_{val}', val),
+            ])
+
+            # create node that reflects the weight if the bit is true, or 0
+            bits_n.append(If(f'dist_{value_id}{i}', operands=[node, const_n, const_0]))
+        int_n = Sum(f'dist_int_{value_id}', operands=bits_n)
+
+        return (
+            it.chain(
+                (PlaceHolder(node.name) for node in wanted_nodes),
+                consts_n,
+                bits_n,
+                [int_n],
+            ),
+            int_n.name,
+        )
