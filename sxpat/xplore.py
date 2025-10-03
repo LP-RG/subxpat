0fbe537c0401aeaf825953fb923f352304225c17
Marco Biasion <biasion.marco.2001@gmail.com>
2025-10-03 18:02:55 +0200

    #172: cleanup

--- sxpat/xplore.py
+++ sxpat/xplore.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Callable, Dict, Iterable, Iterator, List, Tuple
+from typing import Dict, Iterable, Iterator, List, Tuple
 
 from tabulate import tabulate
 import functools as ft
