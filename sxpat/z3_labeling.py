92e719ec97b21bd38d94175344c391defe55589b
Marco Biasion <biasion.marco.2001@gmail.com>
2025-10-03 19:06:33 +0200

    #140: minor changes

--- sxpat/z3_labeling.py
+++ sxpat/z3_labeling.py
@@ -1,4 +1,5 @@
 from typing import Dict
+from typing_extensions import override
 
 import re
 import os
@@ -9,9 +10,9 @@ from Z3Log.config.path import *
 from Z3Log.config.config import *
 
 
- 
 class Z3solverRef(Z3solver):
 
+    @override
     def label_circuit(self, constant_value: bool = False, partial: bool = False, et: int = -1):
         self.experiment = SINGLE
         self.set_strategy(MONOTONIC)
@@ -19,14 +20,11 @@ class Z3solverRef(Z3solver):
         predecessors_to_label = list(
             self.graph.graph.predecessors(self.graph.output_dict[1]))
 
-
         if (partial or self.partial) and et != -1:
 
             already_labeled = set()
             output_dict = self.labeling_graph.output_dict
             sorted_output_dict = dict(sorted(output_dict.items()))
-
-
 
             for output_idx in sorted_output_dict:
                 if 2 ** output_idx > et:
@@ -47,7 +45,6 @@ class Z3solverRef(Z3solver):
                                 predecessors_to_label.extend(list(self.labeling_graph.graph.predecessors(gate)))
             self.run_z3pyscript_labeling()
             return self.import_labels(constant_value)
-            
 
         else:
             for key in self.labeling_graph.gate_dict:
@@ -61,6 +58,7 @@ class Z3solverRef(Z3solver):
             self.run_z3pyscript_labeling()
             return self.import_labels(constant_value)
 
+    @override
     def express_monotonic_while_loop(self):
         loop = ''
 
@@ -103,6 +101,7 @@ class Z3solverRef(Z3solver):
 
         return loop
 
+    @override
     def express_monotonic_while_loop_sat(self):
         if_sat = ''
         if_sat += f"{TAB}if response == sat:\n" \
@@ -111,14 +110,13 @@ class Z3solverRef(Z3solver):
                   f"{TAB}{TAB}returned_model = s.model()\n" \
                   f"{TAB}{TAB}print(f'{{returned_model = }}')\n"
         if self.metric == WAE or self.metric == WRE:
-                  f"{TAB}{TAB}print(f\"{{returned_model[f_exact].else_value() = }}\")\n" \
-                  f"{TAB}{TAB}print(f\"{{returned_model[f_approx].else_value() = }}\")\n" \
-                  f"{TAB}{TAB}print(f\"{{returned_model[f_error].else_value() = }}\")\n"
+            f"{TAB}{TAB}print(f\"{{returned_model[f_exact].else_value() = }}\")\n" \
+                f"{TAB}{TAB}print(f\"{{returned_model[f_approx].else_value() = }}\")\n" \
+                f"{TAB}{TAB}print(f\"{{returned_model[f_error].else_value() = }}\")\n"
 
         if self.metric == WAE:
             if_sat += f"{TAB}{TAB}returned_value = handle.upper()\n" \
                       f"{TAB}{TAB}returned_value_reval = abs(int(returned_model[f_error].else_value().as_long()))\n"
-
 
         if_sat += f"{TAB}{TAB}if returned_value == returned_value_reval:\n" \
                   f"{TAB}{TAB}{TAB}print(f'double-check is passed!')\n" \
@@ -134,11 +132,12 @@ class Z3solverRef(Z3solver):
                   f"{TAB}{TAB}stats['sat_runtime'] += (end_iteration - start_iteration)\n" \
                   f"{TAB}{TAB}stats['jumps'].append(returned_value)\n"
 
-        if_sat +=   f"{TAB}{TAB}foundWCE = True\n" \
-                    f"{TAB}{TAB}stats['wce'] = stats['et']\n"
+        if_sat += f"{TAB}{TAB}foundWCE = True\n" \
+            f"{TAB}{TAB}stats['wce'] = stats['et']\n"
 
         return if_sat
 
+    @override
     def express_stats(self):
         stats = ''
         stats += f"end_whole = time.time()\n" \
@@ -156,8 +155,8 @@ class Z3solverRef(Z3solver):
                  f"{TAB}csvwriter.writerow(['Jumps', stats['jumps']])\n"
         return stats
 
-    def import_labels(self, constant_value: bool=False):
-
+    @override
+    def import_labels(self, constant_value: bool = False):
 
         label_dict: Dict[str, int] = {}
         double_check_dict: Dict[str, bool] = {}
@@ -183,7 +182,7 @@ class Z3solverRef(Z3solver):
 
                             label_dict[gate_label] = gate_wce
                             self.append_label(gate_label, gate_wce)
-                        
+
                         if re.search('double_check', line[0]):
                             double_check = bool(line[1])
                             double_check_dict[gate_label] = double_check
