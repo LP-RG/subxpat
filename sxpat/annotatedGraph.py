b760fe2819f95c6e234fa9d6dc9ef901104cb68a
lollospadalaser <spadalorenzo04@gmail.com>
2025-10-23 15:00:09 +0200

    #174: added additional solver call in case subgraph isn't maximized

--- sxpat/annotatedGraph.py
+++ sxpat/annotatedGraph.py
@@ -2026,7 +2026,7 @@ class AnnotatedGraph(Graph):
 
         opt.add(And(feasibility_constraints))
 
-        opt.maximize(Sum(max_nodes))
+        h = opt.maximize(Sum(max_nodes))
 
         # inputs = Int('inputs')
         # outputs = Int('outputs')
@@ -2049,6 +2049,19 @@ class AnnotatedGraph(Graph):
             n_nodes = 0
             pprint.success("subgraph found -> SAT", end='')
             m = opt.model()
+            model_maximized = m.eval(Sum(max_nodes), model_completion=True).as_long()
+            correct_maximum = h.upper().as_long()
+            
+            if correct_maximum != model_maximized:
+                pprint.info2("\nmodel isn't maximized, running another solver call")
+                opt.add(Sum(max_nodes) == correct_maximum)
+                start = time.perf_counter()
+                if opt.check() == sat:
+                    print(f'Subgraph_extraction_second_call_time = {time.perf_counter() - start}')
+                    m = opt.model()
+                else:
+                    raise Exception("Impossible, error in z3py")
+
             # print(f'{m = }')
             for t in m.decls():
                 # print(f'{type(t) = }')
@@ -2303,7 +2316,7 @@ class AnnotatedGraph(Graph):
 
         opt.add(feasibility_sum <= BitVecVal(feasibility_threshold, NUM_BITS))
 
-        opt.maximize(Sum(max_nodes))
+        h = opt.maximize(Sum(max_nodes))
 
         # inputs = Int('inputs')
         # outputs = Int('outputs')
@@ -2326,6 +2339,19 @@ class AnnotatedGraph(Graph):
             n_nodes = 0
             pprint.success("subgraph found -> SAT", end='')
             m = opt.model()
+            model_maximized = m.eval(Sum(max_nodes), model_completion=True).as_long()
+            correct_maximum = h.upper().as_long()
+            
+            if correct_maximum != model_maximized:
+                pprint.info2("\nmodel isn't maximized, running another solver call")
+                opt.add(Sum(max_nodes) == correct_maximum)
+                start = time.perf_counter()
+                if opt.check() == sat:
+                    print(f'Subgraph_extraction_second_call_time = {time.perf_counter() - start}')
+                    m = opt.model()
+                else:
+                    raise Exception("Impossible, error in z3py")
+
             # print(f'{m = }')
             for t in m.decls():
                 # print(f'{type(t) = }')
