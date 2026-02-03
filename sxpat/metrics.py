ae2a17e3695f0dfd725f6724a116538e1ccebce9
Marco Biasion <biasion.marco.2001@gmail.com>
2026-02-03 15:15:11 +0100

    fixed metrics not correctly caching results

--- sxpat/metrics.py
+++ sxpat/metrics.py
@@ -64,54 +64,10 @@ class MetricsEstimator:
 
     @classmethod
     def estimate_metrics(cls, paths: Paths.Synthesis, verilog_path: str, cached: bool = False) -> Metrics:
-        # compute names and paths
-        metrics_verilog_path = f'{verilog_path[:-2]}_for_metrics.v'
-        module_name = cls._extract_module_name(verilog_path)
-
-        # > define commands
-        # yosys command to get area and to generate metrics verilog
-        yosys_command = cls.YOSYS_BASE_COMMAND.format(
-            # circuit
-            verilog_path=verilog_path,
-            metrics_verilog_path=metrics_verilog_path,
-            # config
-            lib_path=paths.cell_library,
-            abc_script_path=paths.abc_script,
-        )
-        # sta command to get delay and power
-        sta_command = cls.STA_BASE_COMMAND.format(
-            # circuit
-            metrics_verilog_path=metrics_verilog_path,
-            module_name=module_name,
-            # config
-            lib_path=paths.cell_library,
-        )
-
-        # > execute commands
-        yosys_result = subprocess.run([sxpatconfig.YOSYS, '-QT'],
-                                      input=yosys_command, text=True,
-                                      stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        sta_result = subprocess.run([sxpatconfig.OPENSTA, '-no_splash'],
-                                    input=sta_command, text=True,
-                                    stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-        # > guards for failures
-        if yosys_result.returncode != 0: raise Exception(f'Yosys ERROR!\n{yosys_result.stderr}')
-        if sta_result.returncode != 0: raise Exception(f'OpenSTA ERROR!\n{sta_result.stderr}')
-
-        # > parse results
-        # area
-        if m := cls.AREA_ANY_PATTERN.search(yosys_result.stdout): area = float(m.group(1))
-        elif m := cls.AREA_ZERO_PATTERN.search(yosys_result.stdout): area = 0.0
-        else: raise Exception('Yosys ERROR!\nNo useful information in the stats log!')
-        # power
-        if m := cls.POWER_PATTERN.search(sta_result.stdout): power = float(m.group(1))
-        else: power = 0.0
-        # delay
-        if m := cls.DELAY_PATTERN.search(sta_result.stdout): delay = float(m.group(1))
-        else: delay = 0.0
-
-        return cls.Metrics(area, power, delay)
+        if cached:
+            return cls._estimate_metrics_cached(paths, verilog_path)
+        else:
+            return cls._estimate_metrics(paths, verilog_path)
 
     @classmethod
     def _estimate_metrics(cls, paths: Paths.Synthesis, verilog_path: str) -> Metrics:
