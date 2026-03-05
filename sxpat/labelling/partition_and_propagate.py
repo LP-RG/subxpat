919c3c2bdaa06c02043a27d62bfa53cd01d64a35
Xiaozihan Wang <xiaozihan.wang@usi.ch>
2026-03-05 22:40:44 +0100

    start step 2: Derivation of the propagation matrix

--- sxpat/labelling/partition_and_propagate.py
+++ sxpat/labelling/partition_and_propagate.py
@@ -16,7 +16,17 @@ def get_root(i, p_map):
         i = new_p
     return root
 
-
+# input:op is string; bits is list of 0/1 [0,1]
+def apply_logic(op, bits):
+    if op == 'AND':
+        return 1 if all(bits) else 0
+    elif op == 'OR':
+        return 1 if any(bits) else 0
+    elif op == 'NOT':
+        return 0 if bits[0] == 1 else 1
+    elif op == 'XOR':
+        return sum(bits) % 2
+    return 0
 
 
 def compute(graph: nx.digraph.DiGraph) -> Mapping[str, int]:
@@ -84,10 +94,12 @@ def compute(graph: nx.digraph.DiGraph) -> Mapping[str, int]:
 
      # Brute-force decomposition: If the input count |I_s| > T_I of a subgraph, directly de compose all nodes in this subgraph, returning it to a state of "one node per subgraph". (This can be improved.)
     # TODO:思考其他策略
+    # 这里是不是会重复？
     for node in graph.nodes:
         root = get_root(node, parent)
         
         if len(subgraph_inputs[root]) > TI_LIMIT:
+             parent[node] = node
              graph.nodes[node]['subgraph_id'] = node
         else:
              graph.nodes[node]['subgraph_id'] = root
@@ -111,17 +123,20 @@ def compute(graph: nx.digraph.DiGraph) -> Mapping[str, int]:
             outputs_of_subgraph[sub_id].add(node)
 
 
-    
-
-
-
-
-
-
-        
-
     # step 2: Derivation of the propagation matrix (section 3.3)
-    ...
+    # Get all subgraph_id
+    all_subgraph_ids = set(nx.get_node_attributes(graph, 'subgraph_id').value())
+
+    # For every subgrapg,we need to calculate the matrix?how to store the matrix
+    for sub_id in all_subgraph_ids:
+    # 1. 找出所有属于这个 sub_id 的节点
+    #  Find all nodes of sub_id and input/output node of the graph
+         nodes_in_sub = [n for n in graph.nodes if graph.nodes[n]['subgraph_id'] == sub_id]
+         sub_inputs = sorted(list(inputs_of_subgraph[sub_id]))
+         sub_outputs = sorted(list(outputs_of_subgraph[sub_id]))
+
+    # TODO:Is there any method we can use to find the output from the input?
+
 
     # step 3: Propagation
     ...
