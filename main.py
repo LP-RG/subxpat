ac048884df19ca2e49b7411330e62e241cbe620c
Marco Biasion <biasion.marco.2001@gmail.com>
2026-03-10 03:39:32 +0100

    #170: streamlined, updated and deprecated some content of main.py

--- main.py
+++ main.py
@@ -1,53 +1,57 @@
-from sxpat.specifications import Specifications
 from sxpat.utils.print import pprint
 
 
 def main():
+    # > parse arguments
+    from sxpat.specifications import Specifications
     specs_obj = Specifications.parse_args()
-    print(f'{specs_obj = }')
+    # print(f'{specs_obj = }')
 
-    if specs_obj.plot: plot(specs_obj)
-    else: run(specs_obj)
-
-
-def plot(specs_obj: Specifications) -> None:
-    from sxpat.stats import Stats
-
-    pprint.info2('generating plots from matching data')
-    stats_obj = Stats(specs_obj)
-    stats_obj.gather_results()
-
-
-def run(specs_obj: Specifications) -> None:
-    # select wanted directories
+    # > select wanted directories
     from sxpat.config import paths as sxpatpaths
-    from Z3Log.config import path as z3logpath
-    #
+    # from Z3Log.config import path as z3logpath
+    # legacy:
     WANTED_DIRECTORIES = [
-        sxpatpaths.OUTPUT_PATH['ver'][0],
+        # sxpatpaths.OUTPUT_PATH['ver'][0],
         sxpatpaths.OUTPUT_PATH['gv'][0],
         sxpatpaths.OUTPUT_PATH['z3'][0],
-        sxpatpaths.OUTPUT_PATH['report'][0],
-        z3logpath.TEST_PATH['tb'][0],
+        # sxpatpaths.OUTPUT_PATH['report'][0],
+        # z3logpath.TEST_PATH['tb'][0],
     ]
 
-    # create/empty the wanted directories
+    # > create/empty the wanted directories
     from sxpat.utils.filesystem import FS
-    # create if missing
+    # legacy: create if missing
     for dir in WANTED_DIRECTORIES: FS.mkdir(dir)
-    # clean if wanted
+    # legacy: clean if wanted
     if specs_obj.clean:
         pprint.info2('removing final and intermediary data')
         for dir in WANTED_DIRECTORIES: FS.emptydir(dir)
-
-    # run system
-    from sxpat.xplore import explore_grid
-    from sxpat.utils.storage import LiveStorage
-    # create sorage for run statistics
-    run_stats_storage = LiveStorage(f'output/new_storage/{specs_obj.run_id}.csv')
-    print('run statistics storage created at:', run_stats_storage.destination)
     #
-    explore_grid(specs_obj, run_stats_storage)
+    FS.mkdir(specs_obj.path.run.base_folder)
+    FS.mkdir(specs_obj.path.run.verilog)
+    FS.mkdir(specs_obj.path.run.graphviz)
+    FS.mkdir(specs_obj.path.run.solver_scripts)
+    FS.mkdir(specs_obj.path.run.temporary)
+
+    # > prepare storage
+    from sxpat.utils.storage import LiveStorage
+    import csv
+    # initialize run stats storage
+    specs_obj.stats_storage = LiveStorage(specs_obj.path.run.run_stats)
+    print('run stats storage created at:', specs_obj.path.run.run_stats)
+    #
+    with open(specs_obj.path.run.arguments, 'w') as args_file:
+        csv_writer = csv.writer(args_file)
+        csv_writer.writerow(['argument', 'value'])
+        csv_writer.writerows(specs_obj.constant_fields.items())
+
+    # > run system
+    from sxpat.xplore import explore_grid
+    #
+    with specs_obj.stats_storage:
+        # input('ready to explore')
+        explore_grid(specs_obj)
 
 
 if __name__ == '__main__':
