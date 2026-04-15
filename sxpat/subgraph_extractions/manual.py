dc1a33e83b949a43049677d88ef7357973f3a7e6
Marco Biasion <biasion.marco.2001@gmail.com>
2026-04-15 18:05:34 +0200

    #43: minor refactoring and fixes

--- sxpat/subgraph_extractions/manual.py
+++ sxpat/subgraph_extractions/manual.py
@@ -2,6 +2,7 @@ from typing import Iterable, Sequence
 
 import os.path
 import networkx as nx
+from sxpat.utils.graph import is_selection_convex
 
 from sxpat.specifications import Specifications
 
@@ -9,7 +10,7 @@ from sxpat.specifications import Specifications
 def extract(graph: nx.DiGraph, specs: Specifications) -> Sequence[str]:
     before_path = os.path.join(specs.path.run.graphviz, 'manual_subgraph_extraction.gv')
     after_path = os.path.join(specs.path.run.graphviz, 'manual_subgraph_extraction_confirmation.gv')
-    
+
     # show graph
     export(graph, before_path)
     print(f'please open file "{before_path}" to view the graph.')
@@ -24,7 +25,7 @@ def extract(graph: nx.DiGraph, specs: Specifications) -> Sequence[str]:
             continue
 
         # check convexity
-        if not is_subgraph_convex(graph, selected_nodes):
+        if not is_selection_convex(graph, selected_nodes):
             print('the selected nodes do not form a convex subgraph')
             continue
 
@@ -52,33 +53,3 @@ def export(graph: nx.DiGraph, path: str, colored: Iterable[str] = ()):
                 f.write(f'"{n}" [label="{n}"];\n')
         for u, v in graph.edges: f.write(f'"{u}" -> "{v}";\n')
         f.write('}')
-
-
-def is_subgraph_convex(graph: nx.DiGraph, selected_nodes: Iterable[str]) -> bool:
-    selected_nodes = frozenset(selected_nodes)
-
-    for source in selected_nodes:
-        visited = set()
-        stack = [(source, False)]
-
-        while stack:
-            node, exited = stack.pop()
-
-            for neighbor in graph.successors(node):
-                if neighbor in visited: continue
-                visited.add(neighbor)
-
-                # if the destination is a subgraph node
-                if neighbor in selected_nodes:
-                    # terminate if the subgraph is not convex
-                    if exited: return False
-                    # continue to next neighbour
-                    # we do not care of path beyond it as it will be done in its own dfs run
-                    else: continue
-
-                # if the destination is outside the subgraph
-                else:
-                    # remember that we exited and continue
-                    stack.append((neighbor, True))
-
-    return True
