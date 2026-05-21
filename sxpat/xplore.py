6d09b52b97ae859103fd5fc2045eb7133e05bfe6
lollospadalaser <spadalorenzo04@gmail.com>
2026-05-21 17:17:01 +0200

    tests: always saving subgraphs

--- sxpat/xplore.py
+++ sxpat/xplore.py
@@ -267,10 +267,9 @@ def explore_grid(specs_obj: Specifications):
         )
         print(f'subgraph_extraction_time = {_time}')
         # logging
-        if specs_obj.debug: 
-            specs_obj.stats_storage.stage(subgraph_dot=os.path.relpath(current_graph.subgraph_out_path, specs_obj.path.run.base_folder))
-            current_graph.export_annotated_graph()
-            print(f'subgraph exported at {current_graph.subgraph_out_path}')
+        specs_obj.stats_storage.stage(subgraph_dot=os.path.relpath(current_graph.subgraph_out_path, specs_obj.path.run.base_folder))
+        current_graph.export_annotated_graph()
+        print(f'subgraph exported at {current_graph.subgraph_out_path}')
 
         # guard: skip if no subgraph was found
         if not subgraph_is_available:
