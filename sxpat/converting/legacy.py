d45d9627689705168fd4f4ce85bef000d3d0ee3a
Marco Biasion <biasion.marco.2001@gmail.com>
2026-06-30 15:27:07 +0200

    implemented digraph to IOGraph+ conversion

--- sxpat/converting/legacy.py
+++ sxpat/converting/legacy.py
@@ -1,11 +1,20 @@
-from sxpat.annotatedGraph import AnnotatedGraph
+from typing import Iterable, Mapping
+
+import networkx as nx
+
+# from sxpat.annotatedGraph import AnnotatedGraph
 
 from sxpat.graph import IOGraph, SGraph
 from sxpat.graph.node import BoolVariable, BoolConstant, And, Not, Identity
 from sxpat.utils.functions import str_to_bool
 
 
-__all__ = ['iograph_from_legacy', 'sgraph_from_legacy']
+# __all__ = ['iograph_from_legacy', 'sgraph_from_legacy']
+__all__ = [
+    'iograph_from_digraph',
+    'iograph_with_weights',
+    'iograph_to_sgraph',
+]
 
 
 def _nodes_from_inner_legacy(inner_graph):
@@ -33,13 +42,69 @@ def _nodes_from_inner_legacy(inner_graph):
     return nodes
 
 
-def iograph_from_legacy(l_graph: AnnotatedGraph) -> IOGraph:
+def iograph_from_legacy(l_graph) -> IOGraph:
     return IOGraph(_nodes_from_inner_legacy(l_graph.graph),
                    l_graph.input_dict.values(),
                    l_graph.output_dict.values())
 
 
-def sgraph_from_legacy(l_graph: AnnotatedGraph) -> SGraph:
+def sgraph_from_legacy(l_graph) -> SGraph:
     return SGraph(_nodes_from_inner_legacy(l_graph.subgraph),
                   l_graph.input_dict.values(),
                   l_graph.output_dict.values())
+
+
+def iograph_from_digraph(clean_digraph: nx.DiGraph) -> IOGraph:
+    gtypes = {'not': Not, 'and': And}
+
+    # construct nodes and extract inputs/outputs
+    nodes = list()
+    inputs_names = list()
+    outputs_names = list()
+    for (node, attrs) in clean_digraph.nodes(True):
+        ntype = attrs.get('type')
+
+        if ntype == 'input':
+            inputs_names.append(node)
+            nodes.append(BoolVariable(node))
+        elif ntype == 'output':
+            outputs_names.append(node)
+            nodes.append(Identity(
+                node,
+                clean_digraph.predecessors(node),  # type: ignore
+            ))
+        elif ntype == 'gate':
+            cls = gtypes[attrs.get('label')]
+            nodes.append(cls(
+                node,
+                clean_digraph.predecessors(node),  # type: ignore
+            ))
+        elif ntype == 'constant':
+            nodes.append(BoolConstant(
+                node,
+                str_to_bool(attrs.get('label')),
+            ))
+        else:
+            raise RuntimeError(f'Unable to parse node {node} from DiGraph (attributes={attrs})')
+
+    # construct graph
+    return IOGraph(nodes, inputs_names, outputs_names)
+
+
+def iograph_with_weights(graph: IOGraph, weights: Mapping[str, int]) -> IOGraph:
+    return graph.copy(
+        node.copy(weight=weights.get(node.name, None))
+        for node in graph.nodes
+    )
+
+
+def iograph_to_sgraph(graph: IOGraph, subgraph_nodes: Iterable[str]) -> SGraph:
+    subgraph_nodes = frozenset(subgraph_nodes)
+    return SGraph(
+        (
+            node.copy(in_subraph=node.name in subgraph_nodes)
+            for node in graph.nodes
+        ),
+        graph.inputs_names,
+        graph.outputs_names,
+    )
