588097bc7a004813a7e2e37ab336b96ab49476ee
Marco Biasion <biasion.marco.2001@gmail.com>
2026-06-30 15:21:54 +0200

    replaced name extraction for circuit filepath

--- sxpat/metrics.py
+++ sxpat/metrics.py
@@ -5,9 +5,9 @@ import subprocess
 import functools as ft
 
 from .config import config as sxpatconfig
+from sxpat.utils.names import extract_name
 from sxpat.utils.decorators import make_utility_class
 from sxpat.specifications import Paths
-from Z3Log_patched.utils import get_pure_name
 
 
 __all__ = ['MetricsEstimator']
@@ -93,7 +93,7 @@ class MetricsEstimator:
         temporary_path: str,
     ) -> Metrics:
         # compute names and paths
-        circuit_name = get_pure_name(circuit_in_verilog_path)
+        circuit_name = extract_name(circuit_in_verilog_path)
         metrics_verilog_path = f'{temporary_path}/{circuit_name}_for_metrics.v'
         module_name = cls._extract_module_name(circuit_in_verilog_path)
 
