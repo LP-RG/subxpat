5f4900b04a43c7127e1cde62f7eee8f9ffa20efd
Marco Biasion <biasion.marco.2001@gmail.com>
2026-07-08 18:49:14 +0200

    cleanup

--- sxpat/converting/legacy.py
+++ sxpat/converting/legacy.py
@@ -2,56 +2,16 @@ from typing import Iterable, Mapping
 
 import networkx as nx
 
-# from sxpat.annotatedGraph import AnnotatedGraph
-
 from sxpat.graph import IOGraph, SGraph
 from sxpat.graph.node import BoolVariable, BoolConstant, And, Not, Identity
 from sxpat.utils.functions import str_to_bool
 
 
-# __all__ = ['iograph_from_legacy', 'sgraph_from_legacy']
 __all__ = [
     'iograph_from_digraph',
     'iograph_with_weights',
     'iograph_to_sgraph',
 ]
-
-
-def _nodes_from_inner_legacy(inner_graph):
-    nodes = list()
-    for (name, value) in inner_graph.nodes(True):
-        # get features
-        label = value.get('label')
-        weight = value.get('weight', None)
-        in_subgraph = bool(value.get('subgraph', False))
-        operands = inner_graph.predecessors(name)
-
-        # create node
-        if label.startswith('in'):  # input
-            nodes.append(BoolVariable(name, weight, in_subgraph))
-        elif label.startswith('out'):  # output
-            nodes.append(Identity(name, operands, weight, in_subgraph))
-        elif label in ('and', 'not'):  # and/not
-            cls = {'not': Not, 'and': And}[label]
-            nodes.append(cls(name, operands, weight, in_subgraph))
-        elif label in ('FALSE', 'TRUE'):  # constant
-            nodes.append(BoolConstant(name, str_to_bool(label), weight, in_subgraph))
-        else:
-            raise RuntimeError(f'Unable to parse node {name} from AnnotatedGraph ({value})')
-
-    return nodes
-
-
-def iograph_from_legacy(l_graph) -> IOGraph:
-    return IOGraph(_nodes_from_inner_legacy(l_graph.graph),
-                   l_graph.input_dict.values(),
-                   l_graph.output_dict.values())
-
-
-def sgraph_from_legacy(l_graph) -> SGraph:
-    return SGraph(_nodes_from_inner_legacy(l_graph.subgraph),
-                  l_graph.input_dict.values(),
-                  l_graph.output_dict.values())
 
 
 def iograph_from_digraph(clean_digraph: nx.DiGraph) -> IOGraph:
@@ -88,7 +48,11 @@ def iograph_from_digraph(clean_digraph: nx.DiGraph) -> IOGraph:
             raise RuntimeError(f'Unable to parse node {node} from DiGraph (attributes={attrs})')
 
     # construct graph
-    return IOGraph(nodes, sorted(inputs_names, key= lambda x : int(x[2:])), sorted(outputs_names, key= lambda x : int(x[3:])))
+    return IOGraph(
+        nodes,
+        sorted(inputs_names, key=lambda x: int(x[2:])),
+        sorted(outputs_names, key=lambda x: int(x[3:])),
+    )
 
 
 def iograph_with_weights(graph: IOGraph, weights: Mapping[str, int]) -> IOGraph:
