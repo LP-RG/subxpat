14e281dfb64afbca6a73c1a82b6ab1ff680534eb
zellei <zellei@usi.ch>
2026-07-16 14:44:42 +0200

    automated tests just for adders under a certain output size

--- automated_solvers_test.py
+++ automated_solvers_test.py
@@ -1,8 +1,10 @@
 from typing import Sequence
-from multiprocessing.pool import ThreadPool
+from concurrent.futures import ThreadPoolExecutor
 import subprocess
 import os
+import re
 
+output_size = re.compile(r'o(\d+)')
 
 def get_paths() -> Sequence[str]:
     path = "benchmarks//v"
@@ -10,19 +12,20 @@ def get_paths() -> Sequence[str]:
     return [
             "benchmarks/v/" + circuit 
             for circuit in circuits
+            if circuit.startswith("adder") and int(output_size.search(circuit)[1]) <= 49 
            ]
 
 def start_execution(benchmark_path: str) -> None:
     subprocess.run(["python3", "main.py", benchmark_path, "--subxpat", 
                     "--max-lpp=8", "--max-ppo=10", "--imax=6", "--omax=3", 
-                    "--max-error=8", "--no-partial-labeling", "--debug"])
+                    f"--max-error={2**(int(output_size.search(benchmark_path)[1]) - 2)}", 
+                    "--no-partial-labeling", "--debug"])
 
 def main():
     benchmark_paths = get_paths()
-    processes = 3
-    with ThreadPool(processes) as pool:
+    processes = 10
+    with ThreadPoolExecutor(processes) as pool:
         pool.map(lambda x: (x, start_execution(x)), benchmark_paths)
-
 
 
 if __name__ == '__main__': main()
