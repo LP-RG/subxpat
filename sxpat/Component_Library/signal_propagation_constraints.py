fc97ac85048fb08a9b37ab4e7bd60191d6a98e91
Andrei Pruneanu <pruneanuandrei2006@gmail.com>
2026-07-20 12:02:53 +0200

    #202: refactor(annotatedGraph): refactor find_subgraph_sensitivity to use ComponentManager model initialization

--- sxpat/Component_Library/signal_propagation_constraints.py
+++ sxpat/Component_Library/signal_propagation_constraints.py
@@ -7,8 +7,9 @@ class SignalPropagationConstraints:
                                       input_literals, gate_literals, output_literals,
                                       tmp_graph, gate_dict, WEIGHT):
         
-        partition_input_edges = []
-        partition_output_edges = []
+        # List of all the partition edges
+        partition_input_edges = []  # list of all the input edges ([S'D_1 + S'D_2 + ..., ...])
+        partition_output_edges = []  # list of all the output edges ([S_1D' + S_2D' + ..., ...])
         edge_w = {}
         edge_constraint = {}
 
