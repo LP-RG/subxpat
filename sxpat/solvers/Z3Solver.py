5f86aeaf4f9d0c4cf536f64c24d310f24e2d9663
Andrei Pruneanu <pruneanuandrei2006@gmail.com>
2026-08-03 14:10:51 +0200

    #211: fix(solver): map datatype variables to Const for ForAll quantifier support

--- sxpat/solvers/Z3Solver.py
+++ sxpat/solvers/Z3Solver.py
@@ -550,8 +550,8 @@ Z3_BITVEC_NODE_MAPPING = {
 }
 Z3_DATATYPE_NODE_MAPPING = {
     # variables
-    BoolVariable: lambda n, operands, accs: f'NodeSort.bool_var(StringVal(\'{n.name}\'))',
-    IntVariable: lambda n, operands, accs: f'NodeSort.int_var(StringVal(\'{n.name}\'))',
+    BoolVariable: lambda n, operands, accs: f'Const(\'{n.name}\', NodeSort)',
+    IntVariable: lambda n, operands, accs: f'Const(\'{n.name}\', NodeSort)',
     # constants
     BoolConstant: lambda n, operands, accs: f'NodeSort.bool_const(StringVal(\'{n.name}\'), {n.value})',
     IntConstant: lambda n, operands, accs: f'NodeSort.int_const(StringVal(\'{n.name}\'), {n.value})',
