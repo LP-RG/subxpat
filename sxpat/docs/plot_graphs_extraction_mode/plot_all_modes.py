a1da986df87ade068c49fff385c00f10fa251e46
Andrei Pruneanu <pruneanuandrei2006@gmail.com>
2026-08-10 16:02:10 +0200

    #202: style: correct indentation and improve code readability

--- sxpat/docs/plot_graphs_extraction_mode/plot_all_modes.py
+++ sxpat/docs/plot_graphs_extraction_mode/plot_all_modes.py
@@ -4,68 +4,68 @@ import os
 
 all_modes_data = {
     "Mode 1": {
-            'Error Space 4': {'x_post': [37.544], 'y_pre': [37.544], 'color': 'blue'},
-            'Error Space 8': {'x_post': [39.8905], 'y_pre': [31.9124], 'color': 'green'},
-            'Error Space 16': {'x_post': [14.079], 'y_pre': [0], 'color': 'yellow'},
-            'Error Space 32': {'x_post': [0], 'y_pre': [0], 'color': 'orange'},
-            'Error Space 64': {'x_post': [0], 'y_pre': [0], 'color': 'red'}
-        },
-        "Mode 2": {
-            'Error Space 4': {'x_post': [37.544], 'y_pre': [43.1756], 'color': 'blue'},
-            'Error Space 8': {'x_post': [10.3246], 'y_pre': [17.3641], 'color': 'green'},
-            'Error Space 16': {'x_post': [0], 'y_pre': [2.8158], 'color': 'yellow'},
-            'Error Space 32': {'x_post': [0], 'y_pre': [0], 'color': 'orange'},
-            'Error Space 64': {'x_post': [0], 'y_pre': [0], 'color': 'red'}
-        },
-        "Mode 3": {
-            'Error Space 4': {'x_post': [47.8686], 'y_pre': [47.8686], 'color': 'blue'},
-            'Error Space 8': {'x_post': [17.8334], 'y_pre': [17.8334], 'color': 'green'},
-            'Error Space 16': {'x_post': [0], 'y_pre': [6.5702], 'color': 'yellow'},
-            'Error Space 32': {'x_post': [0], 'y_pre': [0], 'color': 'orange'},
-            'Error Space 64': {'x_post': [0], 'y_pre': [0], 'color': 'red'}
-        },
-        "Mode 4": {
-            'Error Space 4': {'x_post': [31.4431], 'y_pre': [21.5878], 'color': 'blue'},
-            'Error Space 8': {'x_post': [31.4431], 'y_pre': [16.4255], 'color': 'green'},
-            'Error Space 16': {'x_post': [1.4079], 'y_pre': [0], 'color': 'yellow'},
-            'Error Space 32': {'x_post': [0], 'y_pre': [0], 'color': 'orange'},
-            'Error Space 64': {'x_post': [0], 'y_pre': [14.079], 'color': 'red'}
-        },
-        "Mode 5": {
-            'Error Space 4': {'x_post': [36.1361], 'y_pre': [36.1361], 'color': 'blue'},
-            'Error Space 8': {'x_post': [22.5264], 'y_pre': [10.7939], 'color': 'green'},
-            'Error Space 16': {'x_post': [1.4079], 'y_pre': [1.4079], 'color': 'yellow'},
-            'Error Space 32': {'x_post': [7.5088], 'y_pre': [7.5088], 'color': 'orange'},
-            'Error Space 64': {'x_post': [0], 'y_pre': [0], 'color': 'red'}
-        },
-        "Mode 55": {
-            'Error Space 4': {'x_post': [163.786], 'y_pre': [137.505], 'color': 'blue'},
-            'Error Space 8': {'x_post': [126.242], 'y_pre': [129.527], 'color': 'green'},
-            'Error Space 16': {'x_post': [94.7986], 'y_pre': [101.369], 'color': 'yellow'},
-            'Error Space 32': {'x_post': [53.9695], 'y_pre': [107.939], 'color': 'orange'},
-            'Error Space 64': {'x_post': [9.386], 'y_pre': [9.386], 'color': 'red'}
-        },
-        "Mode 6": {
-            'Error Space 4': {'x_post': [164.724], 'y_pre': [140.79], 'color': 'blue'},
-            'Error Space 8': {'x_post': [162.847], 'y_pre': [132.343], 'color': 'green'},
-            'Error Space 16': {'x_post': [148.768], 'y_pre': [159.093], 'color': 'yellow'},
-            'Error Space 32': {'x_post': [142.198], 'y_pre': [129.527], 'color': 'orange'},
-            'Error Space 64': {'x_post': [86.3512], 'y_pre': [86.3512], 'color': 'red'}
-        },
-        "Mode 11": {
-            'Error Space 4': {'x_post': [135.628], 'y_pre': [135.628], 'color': 'blue'},
-            'Error Space 8': {'x_post': [118.264], 'y_pre': [117.794], 'color': 'green'},
-            'Error Space 16': {'x_post': [107.47], 'y_pre': [100.43], 'color': 'yellow'},
-            'Error Space 32': {'x_post': [82.5968], 'y_pre': [102.777], 'color': 'orange'},
-            'Error Space 64': {'x_post': [11.7325], 'y_pre': [13.6097], 'color': 'red'}
-        },
-        "Mode 12": {
-            'Error Space 4': {'x_post': [158.623], 'y_pre': [158.623], 'color': 'blue'},
-            'Error Space 8': {'x_post': [146.422], 'y_pre': [146.422], 'color': 'green'},
-            'Error Space 16': {'x_post': [106.062], 'y_pre': [124.365], 'color': 'yellow'},
-            'Error Space 32': {'x_post': [78.8424], 'y_pre': [114.04], 'color': 'orange'},
-            'Error Space 64': {'x_post': [83.5354], 'y_pre': [19.7106], 'color': 'red'}
-        }
+        'Error Space 4': {'x_post': [37.544], 'y_pre': [37.544], 'color': 'blue'},
+        'Error Space 8': {'x_post': [39.8905], 'y_pre': [31.9124], 'color': 'green'},
+        'Error Space 16': {'x_post': [14.079], 'y_pre': [0], 'color': 'yellow'},
+        'Error Space 32': {'x_post': [0], 'y_pre': [0], 'color': 'orange'},
+        'Error Space 64': {'x_post': [0], 'y_pre': [0], 'color': 'red'}
+    },
+    "Mode 2": {
+        'Error Space 4': {'x_post': [37.544], 'y_pre': [43.1756], 'color': 'blue'},
+        'Error Space 8': {'x_post': [10.3246], 'y_pre': [17.3641], 'color': 'green'},
+        'Error Space 16': {'x_post': [0], 'y_pre': [2.8158], 'color': 'yellow'},
+        'Error Space 32': {'x_post': [0], 'y_pre': [0], 'color': 'orange'},
+        'Error Space 64': {'x_post': [0], 'y_pre': [0], 'color': 'red'}
+    },
+    "Mode 3": {
+        'Error Space 4': {'x_post': [47.8686], 'y_pre': [47.8686], 'color': 'blue'},
+        'Error Space 8': {'x_post': [17.8334], 'y_pre': [17.8334], 'color': 'green'},
+        'Error Space 16': {'x_post': [0], 'y_pre': [6.5702], 'color': 'yellow'},
+        'Error Space 32': {'x_post': [0], 'y_pre': [0], 'color': 'orange'},
+        'Error Space 64': {'x_post': [0], 'y_pre': [0], 'color': 'red'}
+    },
+    "Mode 4": {
+        'Error Space 4': {'x_post': [31.4431], 'y_pre': [21.5878], 'color': 'blue'},
+        'Error Space 8': {'x_post': [31.4431], 'y_pre': [16.4255], 'color': 'green'},
+        'Error Space 16': {'x_post': [1.4079], 'y_pre': [0], 'color': 'yellow'},
+        'Error Space 32': {'x_post': [0], 'y_pre': [0], 'color': 'orange'},
+        'Error Space 64': {'x_post': [0], 'y_pre': [14.079], 'color': 'red'}
+    },
+    "Mode 5": {
+        'Error Space 4': {'x_post': [36.1361], 'y_pre': [36.1361], 'color': 'blue'},
+        'Error Space 8': {'x_post': [22.5264], 'y_pre': [10.7939], 'color': 'green'},
+        'Error Space 16': {'x_post': [1.4079], 'y_pre': [1.4079], 'color': 'yellow'},
+        'Error Space 32': {'x_post': [7.5088], 'y_pre': [7.5088], 'color': 'orange'},
+        'Error Space 64': {'x_post': [0], 'y_pre': [0], 'color': 'red'}
+    },
+    "Mode 55": {
+        'Error Space 4': {'x_post': [163.786], 'y_pre': [137.505], 'color': 'blue'},
+        'Error Space 8': {'x_post': [126.242], 'y_pre': [129.527], 'color': 'green'},
+        'Error Space 16': {'x_post': [94.7986], 'y_pre': [101.369], 'color': 'yellow'},
+        'Error Space 32': {'x_post': [53.9695], 'y_pre': [107.939], 'color': 'orange'},
+        'Error Space 64': {'x_post': [9.386], 'y_pre': [9.386], 'color': 'red'}
+    },
+    "Mode 6": {
+        'Error Space 4': {'x_post': [164.724], 'y_pre': [140.79], 'color': 'blue'},
+        'Error Space 8': {'x_post': [162.847], 'y_pre': [132.343], 'color': 'green'},
+        'Error Space 16': {'x_post': [148.768], 'y_pre': [159.093], 'color': 'yellow'},
+        'Error Space 32': {'x_post': [142.198], 'y_pre': [129.527], 'color': 'orange'},
+        'Error Space 64': {'x_post': [86.3512], 'y_pre': [86.3512], 'color': 'red'}
+    },
+    "Mode 11": {
+        'Error Space 4': {'x_post': [135.628], 'y_pre': [135.628], 'color': 'blue'},
+        'Error Space 8': {'x_post': [118.264], 'y_pre': [117.794], 'color': 'green'},
+        'Error Space 16': {'x_post': [107.47], 'y_pre': [100.43], 'color': 'yellow'},
+        'Error Space 32': {'x_post': [82.5968], 'y_pre': [102.777], 'color': 'orange'},
+        'Error Space 64': {'x_post': [11.7325], 'y_pre': [13.6097], 'color': 'red'}
+    },
+    "Mode 12": {
+        'Error Space 4': {'x_post': [158.623], 'y_pre': [158.623], 'color': 'blue'},
+        'Error Space 8': {'x_post': [146.422], 'y_pre': [146.422], 'color': 'green'},
+        'Error Space 16': {'x_post': [106.062], 'y_pre': [124.365], 'color': 'yellow'},
+        'Error Space 32': {'x_post': [78.8424], 'y_pre': [114.04], 'color': 'orange'},
+        'Error Space 64': {'x_post': [83.5354], 'y_pre': [19.7106], 'color': 'red'}
+    }
 }
 
 # Define unique markers for each of the 9 modes
