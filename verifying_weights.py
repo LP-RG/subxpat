24603457c42904dedb94a1e2719895c4b54c7375
tiaramustafa <tiara.mustafa07@gmail.com>
2026-08-12 16:02:12 +0200

    updated verification

--- verifying_weights.py
+++ verifying_weights.py
@@ -46,33 +46,37 @@ def verify_weights(base_log_dir="program_outputs"):
     
     all_passed = True
     failed_nodes = 0
-    
-    for node, beta_data in node_weights.items():
-        if beta == 16:
-                continue
-        
-        if beta not in beta_data:
-            print(f"Node {node} is missing Beta {beta} data. Skipping.")
-            continue
 
-        b16_weights = beta_data[16]
-        b16_val = min(b16_weights)
-        
-        for beta in betas:
-            if beta == 16 or beta not in beta_data:
-                print(f"Node {node} is missing Beta 16 data. Skipping.")
-                continue
-                
-            current_beta_weights = beta_data[beta]
-            current_min = min(current_beta_weights)
+    master_betas = [16,8,4,2,1]
+
+    for master_beta in master_betas:
+    
+        for node, beta_data in node_weights.items():
+            if beta >= master_beta:
+                    continue
             
-            # if there is no intersection between the Beta 16 weight and the current beta's weights => fail and its not <= then the min
-            if not b16_weights.intersection(current_beta_weights) and b16_val > current_min:
-                print(f"[FAIL] Node {node} at Beta {beta}:")
-                print(f"Beta 16 weight {b16_weights} not found in {current_beta_weights}")
-                print(f"Beta 16 weight ({b16_weights}) is strictly greater than the minimum weight in Beta {beta} ({current_min}).")
-                all_passed = False
-                failed_nodes += 1
+            if beta not in beta_data:
+                print(f"Node {node} is missing Beta {beta} data. Skipping.")
+                continue
+
+            b_weights = beta_data[master_betas]
+            b_val = min(b_weights)
+            
+            for beta in betas:
+                if beta == master_beta or beta not in beta_data:
+                    print(f"Node {node} is missing Beta {master_beta} data. Skipping.")
+                    continue
+                    
+                current_beta_weights = beta_data[beta]
+                current_min = min(current_beta_weights)
+                
+                # if there is no intersection between the Beta 16 weight and the current beta's weights => fail and its not <= then the min
+                if not b_weights.intersection(current_beta_weights) and b_val > current_min:
+                    print(f"[FAIL] Node {node} at Beta {beta}:")
+                    print(f"Beta {master_beta} weight {b_weights} not found in {current_beta_weights}")
+                    print(f"Beta {master_beta} weight ({b_weights}) is strictly greater than the minimum weight in Beta {beta} ({current_min}).")
+                    all_passed = False
+                    failed_nodes += 1
     if all_passed:
         print(f"SUCCESS! Verified {len(node_weights)} nodes.")
     else:
