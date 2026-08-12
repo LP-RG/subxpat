0c583157ae8d2f4dc496569a0d7352ce7886458b
Andrei Pruneanu <pruneanuandrei2006@gmail.com>
2026-08-12 14:14:21 +0200

    #211: try to fix the error

--- sxpat/solvers/Z3Solver.py
+++ sxpat/solvers/Z3Solver.py
@@ -605,9 +605,6 @@ Z3_BITVEC_NODE_MAPPING = {
 }
 Z3_DATATYPE_NODE_MAPPING = {
     **Z3_INT_NODE_MAPPING, 
-    BoolVariable: lambda n, operands, accs: f'Node.in_subgraph(nodes[\'{n.name}\'])',
-    IntVariable: lambda n, operands, accs: f'Node.weight(nodes[\'{n.name}\'])',
-    Not: lambda n, operands, accs: f'Not(Node.in_subgraph(nodes[\'{operands[0]}\']))' if not operands[0].startswith('Node.') else f'Not({operands[0]})',
 }
 
 # bool/int to Z3 sorts
