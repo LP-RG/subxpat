8e90d7cdacb0ed76aaba0d475dca4985d2d857fe
tiaramustafa <tiara.mustafa07@gmail.com>
2026-08-24 13:38:21 +0200

    partitioning

--- sxpat/xplore.py
+++ sxpat/xplore.py
@@ -647,10 +647,9 @@ def label_graph(circuit: IOGraph, specs_obj: Specifications) -> Dict[str, int]:
             _time = Timer.now()
 
             #partitioning
+            zone_et_array = None
             if specs_obj.error_partitioning is ErrorPartitioningType.EXPONENTIAL:
-                zone_et_array = iter([2**i for i in range(8)]) 
-            else:
-                zone_et_array = None
+                zone_et_array = [2**i for i in range(8)] 
 
             zone_dict = labeller.label_all_zones(
                 node.name, input1_zone, input2_zone, specs_obj.beta, zone_et_array
