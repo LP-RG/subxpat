0d95fbbbe1b1ac3eee590f5478d90715c96a77f9
Marco Biasion <biasion.marco.2001@gmail.com>
2026-09-21 16:06:31 +0200

    minimal task plan

---
+++ sxpat/utils/formats/aiger.py
@@ -0,0 +1,5 @@
+# per la generazione di aiger files
+# prendi aiger-integration/aiger/circuitDiGraph::gen_circuit_digraph()
+
+# per il loading e parsing degli aiger per produrre un IOGraph
+# prendi pezzi da aiger-integration/aiger/circuitDiGraph::MyAnnotatedGraph, e da sxpat/converting/legacy::iograph_from_digraph()
